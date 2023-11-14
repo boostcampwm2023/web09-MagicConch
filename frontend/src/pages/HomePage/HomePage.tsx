4982c0a5947bf876afc5234f11dc0b00f8875eaf
HeoJiye <heoze1031@gmail.com>
2023-11-14 19:20:25 +0900

    design: tailwind 사용 방식 수정
    - w-[100vw] -> w-screen
    - z-[-1] -> -z-10

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,8 @@ import CustomButton from "@components/CustomButton";
 
 const HomePage = () => {
   return (
-    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center gap-[80px]">
-      <img className="absolute w-full h-full object-cover z-[-1]" src="/bg.png" />
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-[80px]">
+      <img className="absolute w-full h-full object-cover -z-10" src="/bg.png" />
       <img className="w-[214px] h-[214px] animate-shining" src="/moon.png" />
       <div className="flex gap-[36px] mb-[128px]">
         <CustomButton color="active" size="s">AI에게 타로보기</CustomButton>
