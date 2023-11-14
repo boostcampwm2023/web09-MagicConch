18a21d1f6b59db55e238a7bbd6fd2c0b353c7fad
iQuQi <kidscop99@naver.com>
2023-11-14 23:45:01 +0900

    feature: 커스텀 컴포넌트 크기, 색상, 보더 등 보완

--- frontend/src/components/CustomButton/CustomButton.tsx
+++ frontend/src/components/CustomButton/CustomButton.tsx
@@ -1,5 +1,5 @@
 interface ButtonProps {
-  color?: 'active' | 'cancel' | 'disabled' | 'transparent';
+  color?: 'active' | 'cancel' | 'disabled' | 'sub' | 'transparent';
   size?: 's' | 'm' | 'l';
   disabled?: boolean;
   children?: React.ReactNode;
@@ -7,23 +7,24 @@ interface ButtonProps {
 }
 
 const colorMap: Record<string, string> = {
-  active: 'surface-point-alt',
-  cancel: 'text-weak',
+  active: 'surface-point-alt text-white',
+  cancel: 'text-weak text-white',
   disabled: 'surface-disabled',
+  sub: 'surface-alt text-white',
   transparent: 'bg-transparent border-transparent',
 };
 
 const sizeMap: Record<string, string> = {
-  s: 'h-6 display-bold16',
-  m: 'h-14 display-bold16',
-  l: 'h-16 display-bold16',
+  s: 'h-40 display-bold14 leading-18 p-12 min-h-0',
+  m: 'h-50 display-bold16 leading-20 p-16',
+  l: 'h-60 display-bold16 leading-30 p-16',
 };
 
 function CustomButton({ color = 'active', size = 'm', disabled = false, children, handleButtonClicked }: ButtonProps) {
   return (
     <button
       disabled={disabled}
-      className={`btn rounded-full py-2 ${colorMap[color]} ${sizeMap[size]}`}
+      className={`btn rounded-full border-transparent ${colorMap[color]} ${sizeMap[size]}`}
       onClick={handleButtonClicked}
     >
       {children}
