fd51de2793e5fb47421f8e95092faa09ab58645f
HeoJiye <heoze1031@gmail.com>
2023-11-14 19:10:03 +0900

    feat: Header logo 추가, 홈으로 이동 Link 추가
    - router 사용하게 되면서 story 파일은 삭제함

--- frontend/src/components/Header/HeaderLeft.tsx
+++ frontend/src/components/Header/HeaderLeft.tsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function HeaderLeft() {
-  return <div></div>;
+  return (
+    <div className="display-bold16">
+      <Link to="/">마법의 소라고둥</Link>
+    </div>
+  );
 };
