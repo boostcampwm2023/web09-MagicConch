337c51f34eddcefe98038bb035ccceefb9324e6d
HeoJiye <heoze1031@gmail.com>
2023-11-14 18:17:44 +0900

    feat: 홈 화면 배경 설정

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
 interface HomePageProps {}
 
 const HomePage = ({}: HomePageProps) => {
-  return <></>;
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center">
+      <img className="w-full h-full object-cover" src="/bg.png" />
+      <img className="w-[200px] h-[200px] absolute top-[160px]" src="/moon.png" />
+    </div>
+  );
 };
 
 export default HomePage;
