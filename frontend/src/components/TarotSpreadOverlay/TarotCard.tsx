88d0eac147d54f8d27db237c5e8bba7bada40890
HeoJiye <heoze1031@gmail.com>
2023-11-15 14:10:03 +0900

    feat: 임시 타로 뽑기 구현

--- frontend/src/components/TarotSpreadOverlay/TarotCard.tsx
+++ frontend/src/components/TarotSpreadOverlay/TarotCard.tsx
@@ -1,3 +1,29 @@
-export default function TarotCard() {
-  return <div></div>;
+import { useEffect, useState } from 'react';
+
+interface TarotCardProps {
+  idx?: number; // undefined면 뒷면을 의미함
+  onClick?: () => void;
+}
+
+export default function TarotCard({ idx, onClick }: TarotCardProps) {
+  const [img, setImg] = useState<string>('');
+
+  useEffect(() => {
+    // ToDo: react-query api로 이미지를 가져오도록 수정해야 함
+    async function fetchCardImg() {
+      const cardName = idx === undefined ? 'back' : idx.toString().padStart(2, '0');
+      const res = await import(`../../mocks/cards/${cardName}.jpg`);
+      setImg(res.default);
+    }
+    fetchCardImg();
+  }, []);
+
+  return (
+    <img
+      className="w-288 h-480 absolute"
+      src={img}
+      alt="타로 카드 이미지"
+      onClick={onClick}
+    />
+  );
 }
--- frontend/src/components/TarotSpreadOverlay/TarotSpreadOverlay.tsx
+++ frontend/src/components/TarotSpreadOverlay/TarotSpreadOverlay.tsx
@@ -1,3 +1,18 @@
-export default function TarotSpreadOverlay() {
-  return <div></div>;
+import TarotCard from './TarotCard';
+
+interface TarotSpreadProps {
+  pickCard: (idx: number) => void;
+}
+
+export default function TarotSpread({ pickCard }: TarotSpreadProps) {
+  return (
+    <div className="w-screen h-screen absolute z-100 flex justify-center items-center">
+      <img
+        className="w-full h-full object-cover absolute"
+        src="/bgConch.png"
+        alt="밤 하늘의 배경 이미지"
+      />
+      <TarotCard onClick={() => pickCard(Math.floor(Math.random() * 21) + 1)} />
+    </div>
+  );
 }
