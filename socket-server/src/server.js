2a1860d2dae76079a7c41a6caa6d59867a1a6b46
HeoJiye <heoze1031@gmail.com>
2023-11-15 15:24:43 +0900

    stlye: socket server에서 client가 연결이 끊기면 로그 출력

--- socket-server/src/server.js
+++ socket-server/src/server.js
@@ -8,4 +8,8 @@ const io = new Server(3000, {
 
 io.on("connection", (socket) => {
   console.log("New client connected: " + socket.id);
+
+  socket.on("disconnecting", () => {
+    console.log("Client disconnected: " + socket.id);
+  });
 });
