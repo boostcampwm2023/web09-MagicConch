089984fb0f9fd26380fb0ff765f379e7fbc5e441
HeoJiye <heoze1031@gmail.com>
2023-11-15 15:10:26 +0900

    fix: server에서 cors 설정

--- socket-server/src/server.js
+++ socket-server/src/server.js
@@ -1,6 +1,10 @@
 import { Server } from "socket.io";
 
-const io = new Server(3000);
+const io = new Server(3000, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("New client connected: " + socket.id);
