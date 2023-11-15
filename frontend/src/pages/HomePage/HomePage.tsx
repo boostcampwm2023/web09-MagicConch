688bbdc2cb44bf33c075d0f1c60d2c75b1b3d1cb
HeoJiye <heoze1031@gmail.com>
2023-11-15 14:54:47 +0900

    refactor: 홈 화면 페이지 이동 handle 함수 분리

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,14 @@ import CustomButton from '@components/CustomButton';
 const HomePage = () => {
   const navigate = useNavigate();
 
+  const moveAiChat = () => {
+    navigate('/chat/ai');
+  };
+
+  const moveHumanChat = () => {
+    navigate('/chat/human');
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-80">
       <img
@@ -18,9 +26,9 @@ const HomePage = () => {
         alt="빛나는 마법의 소라 고둥"
       />
       <div className="flex gap-36 mb-128 z-1">
-        <CustomButton handleButtonClicked={() => navigate('/chat/ai')}>AI에게 타로보기</CustomButton>
+        <CustomButton handleButtonClicked={moveAiChat}>AI에게 타로보기</CustomButton>
         <CustomButton
-          handleButtonClicked={() => navigate('/chat/human')}
+          handleButtonClicked={moveHumanChat}
           color="dark"
         >
           채팅방 개설하기
