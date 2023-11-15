228bdaf10138cae8111551bc49c87b3beecf545c
iQuQi <kidscop99@naver.com>
2023-11-15 14:07:07 +0900

    feat: 헤더 로고 버튼 수정
    - 크기 수정
    - 로고 이미지 추가

--- frontend/src/components/Header/HeaderLeft.tsx
+++ frontend/src/components/Header/HeaderLeft.tsx
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function HeaderLeft() {
   return (
-    <div className="display-bold16">
-      <Link to="/">마법의 소라고둥</Link>
-    </div>
+    <Link
+      to="/"
+      className="flex display-bold16"
+    >
+      <img
+        className="w-33 h-33 mr-8"
+        src="/logo.png"
+        alt="마법의 소라고둥 로고 이미지"
+      />
+      <span className="leading-35 text-strong">마법의 소라고둥</span>
+    </Link>
   );
-};
+}
