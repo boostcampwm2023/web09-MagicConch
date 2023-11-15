63a10d1fda88dc45822755ca6cffab748f12e331
iQuQi <kidscop99@naver.com>
2023-11-15 14:14:31 +0900

    feat: /chat/ai 페이지 구성
    - 배경 이미지 및 헤더 넣기

--- frontend/src/pages/AIChatPage/AIChatPage.tsx
+++ frontend/src/pages/AIChatPage/AIChatPage.tsx
@@ -1,7 +1,33 @@
+import CustomButton from '@/components/CustomButton';
+import Header from '@/components/Header';
+import { Icon } from '@iconify/react/dist/iconify.js';
+
 interface AIChatPageProps {}
 
 const AIChatPage = ({}: AIChatPageProps) => {
-  return <></>;
+  return (
+    <div className="w-screen h-screen">
+      <Header
+        rightItems={[
+          <CustomButton
+            color="transparent"
+            size="s"
+          >
+            <Icon
+              className="w-22 h-22"
+              icon="carbon:side-panel-close"
+            />
+          </CustomButton>,
+        ]}
+      />
+      <img
+        className="absolute w-full h-full object-cover -z-10"
+        src="/bgConch.png"
+        alt="밤 하늘의 배경 이미지"
+      />
+      <div className="w-full h-full bg-black/70" />
+    </div>
+  );
 };
 
 export default AIChatPage;
