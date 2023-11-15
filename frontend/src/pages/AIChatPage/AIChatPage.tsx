dbf735c21987ea5fe5a54ed199870129a1ae44f9
iQuQi <kidscop99@naver.com>
2023-11-15 16:29:30 +0900

    feat: AIChatPage 화면 구성 & 어두운 배경과 애니메이션 추가 & 테스트용 코드 추가

--- frontend/src/pages/AIChatPage/AIChatPage.tsx
+++ frontend/src/pages/AIChatPage/AIChatPage.tsx
@@ -1,9 +1,12 @@
 import CustomButton from '@/components/CustomButton';
 import Header from '@/components/Header';
-import MessageBox from '@/components/MessageBox/MessageBox';
+import MessageBox from '@/components/MessageBox';
 import { Icon } from '@iconify/react';
 
 interface AIChatPageProps {}
+
+const messageTest =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
 
 const AIChatPage = ({}: AIChatPageProps) => {
   return (
@@ -14,11 +17,11 @@ const AIChatPage = ({}: AIChatPageProps) => {
         alt="밤 하늘의 배경 이미지"
       />
       <img
-        className="w-285 h-285 relative bottom-130"
+        className="w-285 h-285 animate-shining relative bottom-130"
         src="/moon.png"
         alt="빛나는 마법의 소라 고둥"
       />
-      <div className="absolute w-full h-full bg-black/80 animate-fadeIn" />
+      <div className="absolute w-full h-full bg-black/75 animate-fadeIn" />
       <Header
         rightItems={[
           <CustomButton
@@ -33,6 +36,22 @@ const AIChatPage = ({}: AIChatPageProps) => {
           </CustomButton>,
         ]}
       />
+
+      {/* 테스트용 임시 ul */}
+      <ul className="absolute w-800 m-auto">
+        <li>
+          <MessageBox
+            type="left"
+            message={messageTest}
+          />
+        </li>
+        <li>
+          <MessageBox
+            type="right"
+            message={messageTest}
+          />
+        </li>
+      </ul>
     </div>
   );
 };
