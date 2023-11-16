a2ce1d25ad2e5a4355bd8778e74d3b16011b3abf
Song_Minhyung <song961003@gmail.com>
2023-11-16 18:46:14 +0900

    feat: mock, const 파일 분리

---
+++ frontend/__tests__/mocks/resultShareMocks/resultShareMock.ts
@@ -0,0 +1,12 @@
+export const shareUrlMock = {
+  asdflkj12l3k12u32osudfaskfljdklsf: {
+    imageId: '9',
+    text: `
+    [시작]
+    오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의
+    운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세오늘의 운세 오늘의 운세오늘의
+    운세오늘의 운세오늘의 운세오늘의 운`,
+  },
+};
+
+export const shareUrlMockUrl: keyof typeof shareUrlMock = 'asdflkj12l3k12u32osudfaskfljdklsf';
