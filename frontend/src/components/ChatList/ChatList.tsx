35e4097eb8b3205475343471496b7347f63c86f6
iQuQi <kidscop99@naver.com>
2023-11-16 16:22:07 +0900

    feat: chatList 컴포넌트 구현

---
+++ frontend/src/components/ChatList/ChatList.tsx
@@ -0,0 +1,29 @@
+import MessageBox from "../MessageBox";
+
+interface Message {
+  type: 'left' | 'right';
+  message: string;
+  profile?: string;
+}
+
+interface ChatListProps {
+  size?: string;
+  messages: Message[];
+}
+
+function ChatList({size = "760", messages}: ChatListProps) {
+  return (
+    <ul className={`absolute w-${size} h-3/4 overflow-auto`}>
+      {messages.map(({type, message, profile}) => 
+      <li className={`flex pr-20 pb-40 ${type == 'right' ?  'justify-end' : ''}`}>
+        <MessageBox
+          type={type}
+          message={message}
+          profile={profile}
+        />
+      </li>)}
+    </ul>
+  );
+}
+
+export default ChatList;
