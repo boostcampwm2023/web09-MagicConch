f882700d4267d736136ada5729a044bfed02f55f
kimyu0218 <serena35@ewhain.net>
2023-11-16 22:58:45 +0900

    feat: 메인 모듈 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { DatabaseModule } from './common/config/database/database.module';
 import { EventsModule } from './events/events.module';
+import { Member } from './members/entities/member.entity';
+import { MembersModule } from './members/members.module';
+import { MembersService } from './members/members.service';
+import { TarotModule } from './tarot/tarot.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MembersModule,
+    DatabaseModule,
+    ChatModule,
+    TarotModule,
+    TypeOrmModule.forFeature([Member]),
+    TarotModule,
+    EventsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [MembersService],
 })
 export class AppModule {}
