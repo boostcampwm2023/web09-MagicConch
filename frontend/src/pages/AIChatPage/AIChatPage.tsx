b4ca0a427ef324a2698ab5bb21dd21ce672bf14d
HeoJiye <heoze1031@gmail.com>
2023-11-16 19:05:19 +0900

    feat: server에서 받아온 메세지 & client에서 보내는 메세지 띄우기

--- frontend/src/pages/AIChatPage/AIChatPage.tsx
+++ frontend/src/pages/AIChatPage/AIChatPage.tsx
@@ -1,15 +1,32 @@
 import { Icon } from '@iconify/react';
+import { useEffect, useState } from 'react';
+
 import Background from '@components/Background';
 import ChatInput from '@components/ChatInput';
-import ChatList from '@components/ChatList/ChatList';
+import ChatList, { Message } from '@components/ChatList/ChatList';
 import CustomButton from '@components/CustomButton';
 import Header from '@components/Header';
 
-import { sendMessage } from '@business/services/socket';
+import { sendMessage, setMessageEventListener } from '@business/services/socket';
 
 interface AIChatPageProps {}
 
 const AIChatPage = ({}: AIChatPageProps) => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const addMessage = (type: 'left' | 'right', message: string) => {
+    setMessages(messages => [...messages, { type, message }]);
+  };
+
+  const onSubmitMessage = (message: string) => {
+    addMessage('right', message);
+    sendMessage(message);
+  };
+
+  useEffect(() => {
+    setMessageEventListener(message => addMessage('left', message));
+  }, []);
+
   return (
     <Background type="dynamic">
       <Header
@@ -28,28 +45,12 @@ const AIChatPage = ({}: AIChatPageProps) => {
       />
 
       {/* TEST */}
-      <ChatList
-        messages={[
-          { type: 'left', message: '안녕 만나서 반가워 친구야' },
-          {
-            type: 'right',
-            message:
-              '고민이 있는데 들어주겠니? 배가너무 고픈데 뭘 먹어야할지 모르겠어. 답을 알려줘 마법의 소라고둥!고민이 있는데 들어주겠니? 배가너무 고픈데 뭘 먹어야할지 모르겠어. 답을 알려줘 마법의 소라고둥!',
-          },
-          {
-            type: 'left',
-            message: '사케동은 어때?? 담백하고 부드러워서 언제먹어도 질리지 않아!',
-          },
-        ]}
-      />
+      <ChatList messages={messages} />
 
       {/* // TODO 서버에서 AI 데이터를 받아오고 있는 동안 disabled 하기 */}
       <ChatInput
         disabled={false}
-        sendChatMessage={message => {
-          // TODO: 서버로 메시지 전송 & 화면에 메시지 컴포넌트 그리기
-          sendMessage(message);
-        }}
+        sendChatMessage={onSubmitMessage}
       />
     </Background>
   );
