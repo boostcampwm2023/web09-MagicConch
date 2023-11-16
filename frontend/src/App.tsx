71323970efac768aae15b4d2e75048af68741e20
Song_Minhyung <song961003@gmail.com>
2023-11-16 19:20:31 +0900

    style: 쌍따옴표로 됐던거 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import BackgroundMusic from "./components/BackgroundMusic";
-import AIChatPage from "./pages/AIChatPage";
-import HomePage from "./pages/HomePage";
-import HumanChatPage from "./pages/HumanChatPage";
-import ReaderListPage from "./pages/ReaderListPage";
-import ResultSharePage from "./pages/ResultSharePage";
+import AIChatPage from './pages/AIChatPage';
+import HomePage from './pages/HomePage';
+import HumanChatPage from './pages/HumanChatPage';
+import ReaderListPage from './pages/ReaderListPage';
+import ResultSharePage from './pages/ResultSharePage';
+
+import BackgroundMusic from './components/BackgroundMusic';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/chat/ai/:id?" element={<AIChatPage />} />
-        <Route path="/chat/human/:id" element={<HumanChatPage />} />
-        <Route path="/readers" element={<ReaderListPage />} />
-        <Route path="/result/:id" element={<ResultSharePage />} />
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/chat/ai/:id?"
+          element={<AIChatPage />}
+        />
+        <Route
+          path="/chat/human/:id"
+          element={<HumanChatPage />}
+        />
+        <Route
+          path="/readers"
+          element={<ReaderListPage />}
+        />
+        <Route
+          path="/result/:id"
+          element={<ResultSharePage />}
+        />
       </Routes>
       <BackgroundMusic />
     </>
