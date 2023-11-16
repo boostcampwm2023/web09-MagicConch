d207447937c1ee658a456a439cc2d9386aab1715
kimyu0218 <serena35@ewhain.net>
2023-11-16 15:57:27 +0900

    feat: 채팅 컨트롤러에서 가드 사용하도록 수정
    - UseGuard 데코레이터를 이용하여 채팅에서 AuthGuard 사용 명시
    - 메서드마다 있었던, 중복된 if문 제거

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -8,11 +8,12 @@ import {
   Patch,
   Post,
   Req,
-  UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiBody,
   ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiParam,
@@ -20,12 +21,14 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { Request } from 'express';
+import { AuthGuard } from 'src/common/auth/auth.guard';
 import { ChatService } from './chat.service';
 import { ChattingMessageResponseDto } from './dto/chatting-messag-response.dto';
 import { ChattingRoomResponseDto } from './dto/chatting-room-response.dto';
 import { CreateChattingMessageDto } from './dto/create-chatting-message.dto';
 import { UpdateChattingRoomDto } from './dto/update-chatting-room.dto';
 
+@UseGuards(AuthGuard)
 @Controller('chat')
 @ApiTags('✅Chatting API')
 export class ChatController {
@@ -39,9 +42,6 @@ export class ChatController {
   })
   @ApiUnauthorizedResponse({ description: '인증 받지 않은 사용자' })
   async findRooms(@Req() req: Request): Promise<ChattingRoomResponseDto[]> {
-    if (!req.cookies.magicConch) {
-      throw new UnauthorizedException();
-    }
     return await this.chatService.findRoomsById(req.cookies.magicConch);
   }
 
@@ -55,11 +55,7 @@ export class ChatController {
   @ApiUnauthorizedResponse({ description: '인증 받지 않은 사용자' })
   async findMessages(
     @Param('id', ParseUUIDPipe) id: string,
-    @Req() req: Request,
   ): Promise<ChattingMessageResponseDto[]> {
-    if (!req.cookies.magicConch) {
-      throw new UnauthorizedException();
-    }
     return await this.chatService.findMessagesById(id);
   }
 
@@ -73,11 +69,7 @@ export class ChatController {
   async createMessages(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() CreateChattingMessageDto: CreateChattingMessageDto[],
-    @Req() req: Request,
   ): Promise<void> {
-    if (!req.cookies.magicConch) {
-      throw new UnauthorizedException();
-    }
     return await this.chatService.createMessage(id, CreateChattingMessageDto);
   }
 
@@ -86,8 +78,9 @@ export class ChatController {
   @ApiParam({ type: 'uuid', name: 'id' })
   @ApiBody({ type: UpdateChattingRoomDto })
   @ApiOkResponse({ description: '채팅방 제목 수정 성공' })
+  @ApiNotFoundResponse({ description: '존재하지 않는 채팅방 ID' })
   @ApiInternalServerErrorResponse()
-  async update(
+  async updateRoom(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateChattingRoomDto: UpdateChattingRoomDto,
   ): Promise<void> {
@@ -99,7 +92,7 @@ export class ChatController {
   @ApiParam({ type: 'uuid', name: 'id' })
   @ApiOkResponse({ description: '채팅방 삭제 성공' })
   @ApiInternalServerErrorResponse()
-  async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+  async removeRoom(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
     await this.chatService.removeRoom(id);
   }
 }
