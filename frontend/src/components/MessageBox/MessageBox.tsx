db126ce74deb2c016cf7f56584c34dffad6f2d84
iQuQi <kidscop99@naver.com>
2023-11-16 17:24:31 +0900

    chore: 오타 수정

--- frontend/src/components/MessageBox/MessageBox.tsx
+++ frontend/src/components/MessageBox/MessageBox.tsx
@@ -11,7 +11,7 @@ interface MessageBoxProps {
 }
 
 // TODO: tarotId로 서버에서 타로 카드 이미지 정보를 받아와서 src와 alt 채워주기
-// TODO: 조건식 !tarotID -> tarotId로 변경
+// TODO: 조건식 !tarotId -> tarotId로 변경
 // TODO: 프로필 이미지 설정
 
 const MessageBox = ({ tarotId, type, message, profile = '/sponge.png' }: MessageBoxProps) => {
