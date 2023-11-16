f8b9638a7435abd0b7d39158074d055998098660
kimyu0218 <serena35@ewhain.net>
2023-11-16 15:53:55 +0900

    feat: 쿠키 여부를 확인하는 가드 정의
    - 쿠키를 가지고 있지 않은 사용자는 UnauthorizedException 던진다.
    - 즉, 컨트롤러의 각 메서드에서 쿠키 여부를 확인하지 않아도 된다.

---
+++ backend/src/common/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req: Request = context.switchToHttp().getRequest();
+    if (!req.cookies.magicConch) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
