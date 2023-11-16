abeb4d642ec0f8711952f502d32192c930f6ff1b
iQuQi <kidscop99@naver.com>
2023-11-16 17:39:50 +0900

    review: 삼항연산자로 계산되는 긴 스타일 코드는 usememo로 빼주기

--- frontend/src/components/ChatList/ChatList.tsx
+++ frontend/src/components/ChatList/ChatList.tsx
@@ -1,4 +1,6 @@
-import MessageBox from "../MessageBox";
+import { useMemo } from 'react';
+
+import MessageBox from '../MessageBox';
 
 interface Message {
   type: 'left' | 'right';
@@ -11,17 +13,21 @@ interface ChatListProps {
   messages: Message[];
 }
 
-function ChatList({size = "760", messages}: ChatListProps) {
+function ChatList({ size = '760', messages }: ChatListProps) {
   return (
     <ul className={`absolute w-${size} h-3/4 overflow-auto`}>
-      {messages.map(({type, message, profile}) => 
-      <li key={`${type}-${message}`} className={`flex pr-20 pb-40 ${type == 'right' ?  'justify-end' : ''}`}>
-        <MessageBox
-          type={type}
-          message={message}
-          profile={profile}
-        />
-      </li>)}
+      {messages.map(({ type, message, profile }) => (
+        <li
+          key={`${type}-${message}`}
+          className={`flex pr-20 pb-40 ${type == 'right' && 'justify-end'}`}
+        >
+          <MessageBox
+            type={type}
+            message={message}
+            profile={profile}
+          />
+        </li>
+      ))}
     </ul>
   );
 }
