2a4f38e37380b8592ae1711465bbed18b2b9a363
kimyu0218 <serena35@ewhain.net>
2023-11-16 22:52:02 +0900

    feat: 새롭게 생성한 타로 카드팩 Entity 추가

--- backend/src/tarot/tarot.module.ts
+++ backend/src/tarot/tarot.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TarotCardPack } from './entities/tarot-card-pack.entity';
 import { TarotCard } from './entities/tarot-card.entity';
 import { TarotResult } from './entities/tarot-result.entity';
 import { TarotController } from './tarot.controller';
 import { TarotService } from './tarot.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TarotCard, TarotResult])],
+  imports: [TypeOrmModule.forFeature([TarotCard, TarotResult, TarotCardPack])],
   controllers: [TarotController],
   providers: [TarotService],
 })
