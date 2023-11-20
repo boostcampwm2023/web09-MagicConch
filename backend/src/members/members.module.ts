2056ffae382ff9d195ddabf748fc02245169625b
iQuQi <kidscop99@naver.com>
2023-11-21 02:43:16 +0900

    bugfix: 백엔드 오류 임시 막기

--- backend/src/members/members.module.ts
+++ backend/src/members/members.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
-import { MembersController } from './members.controller';
+// import { MembersController } from './members.controller';
 import { MembersService } from './members.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
-  controllers: [MembersController],
+  // controllers: [MembersController],
   providers: [MembersService],
 })
 export class MembersModule {}
