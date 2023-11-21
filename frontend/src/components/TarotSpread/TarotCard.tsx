59efda31a2e4a1e8c67a0af52a1af71a81d3b179
iQuQi <kidscop99@naver.com>
2023-11-21 15:40:50 +0900

    feat: 펼쳐지고 닫히는 애니메이션 추가 및 효과음 넣기

--- frontend/src/components/TarotSpread/TarotCard.tsx
+++ frontend/src/components/TarotSpread/TarotCard.tsx
@@ -12,20 +12,20 @@ const flipSound = new Audio('../../../public/flipCard.mp3');
 export default function TarotCard({ dragging, index, backImg, onClick }: TarotCardProps) {
   const [img, setImg] = useState<string>(backImg);
 
-  const getFlippedCard = () => {
+  const flippedCard = () => {
     // TODO: react-query api로 이미지를 가져오도록 수정해야 함 & 78개 이미지로 수정
     flipSound.play();
     setTimeout(() => setImg(`../../../__tests__/mocks/cards/${(index % 22).toString().padStart(2, '0')}.jpg`), 500);
   };
 
   const clickCard = () => {
-    getFlippedCard();
+    flippedCard();
     onClick();
   };
 
   return (
     <img
-      className={`${!dragging && 'hover:animate-tarotHovering'} animate-tarotLeaving w-full h-full -translate-y-1000`}
+      className={`${!dragging && 'hover:animate-tarotHovering'} animate-tarotLeaving w-full h-full -translate-x-1000`}
       src={img}
       alt="타로 카드 이미지"
       onClick={clickCard}
--- frontend/src/components/TarotSpread/TarotSpread.tsx
+++ frontend/src/components/TarotSpread/TarotSpread.tsx
@@ -4,8 +4,6 @@ import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { shuffledArray } from '@utils/array';
 
-import { rotation } from '@constants/tarotCard';
-
 interface TarotSpreadProps {
   opened: boolean;
   close: () => void;
@@ -16,22 +14,23 @@ const TAROT_COUNT = 78;
 const spreadSound = new Audio('../../../public/spreadCards.mp3');
 
 export default function TarotSpread({ opened, close, pickCard }: TarotSpreadProps) {
-  // TODO: react-query api로 사용자 정의 뒷면 기본 이미지 들고오기
   const [closing, setClosing] = useState<boolean>(!opened);
   const [picked, setPicked] = useState<number>();
   const [dragging, setDragging] = useState<boolean>(false);
 
+  const tarotCardRefs = useRef<HTMLDivElement[]>([]);
   const prevMouseXRef = useRef<number>(0);
   const tarotSpreadRef = useRef<HTMLDivElement>(null);
   const rotationRef = useRef<number>(0);
 
+  // TODO: react-query api로 사용자 정의 뒷면 기본 이미지 들고오기
   const backImg = useMemo(() => `../../../__tests__/mocks/cards/back.png`, []);
   const shuffledCard = useMemo(() => shuffledArray(Array.from({ length: TAROT_COUNT }, (_, idx) => idx)), []);
 
   useEffect(() => {
-    spreadSound.play();
     addEventListener('wheel', ({ deltaX }: WheelEvent) => rotateTarotSpread(deltaX > 0 ? 'left' : 'right'));
     addEventListener('animationend', ({ animationName }: AnimationEvent) => animationName == 'fadeOut' && close());
+    setTimeout(() => spreadTarotCards(), 100);
   }, []);
 
   const dragTarotSpread = (pageX: number) => {
@@ -45,14 +44,30 @@ export default function TarotSpread({ opened, close, pickCard }: TarotSpreadProp
 
     const rotateIndex = rotationRef.current;
     const nextRotateIndex = direction == 'right' ? rotateIndex + 1 : rotateIndex - 1;
-    target.style.transform = `translateX(-50%) rotate(${nextRotateIndex * 0.5}deg)`;
+    target.style.transform = `translateX(-50%) rotate(${nextRotateIndex * 0.6}deg)`;
     rotationRef.current = nextRotateIndex;
+  };
+
+  const spreadTarotCards = () => {
+    spreadSound.play();
+    tarotCardRefs.current.forEach((ref, idx) => {
+      ref.style.transform = `rotate(${idx * 4.6}deg)`;
+      ref.style.transition = 'transform 1s ease-in-out';
+    });
+  };
+
+  const unSpreadTarotCards = () => {
+    spreadSound.play();
+    tarotCardRefs.current.forEach(ref => (ref.style.transform = `rotate(0deg)`));
   };
 
   const clickCard = (id: number) => {
     pickCard(id);
     setPicked(id);
-    setTimeout(() => setClosing(true), 1500);
+    setTimeout(() => {
+      setClosing(true);
+      unSpreadTarotCards();
+    }, 1500);
   };
 
   return (
@@ -63,13 +78,13 @@ export default function TarotSpread({ opened, close, pickCard }: TarotSpreadProp
         onMouseDown={() => setDragging(true)}
         onMouseLeave={() => setDragging(false)}
         onMouseUp={() => setDragging(false)}
-        className="transition-all ease-out absolute w-220 h-400 origin-center top-1200 left-[50%] translate-x-[-50%]"
+        className="transition-all ease-out absolute w-400 h-220 origin-center top-1200 left-[50%] translate-x-[-50%]"
       >
         {shuffledCard.map((id: number, idx: number) => (
           <div
             key={id}
-            className={`${picked && 'pointer-events-none'} ${rotation[(idx + 20) % TAROT_COUNT]} 
-                        absolute w-full h-full`}
+            ref={ref => (tarotCardRefs.current[idx] = ref!)}
+            className={`${picked && 'pointer-events-none'} absolute w-full h-full`}
           >
             <TarotCard
               dragging={dragging}
