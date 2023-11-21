86f4c930cf3952e6904485c019ca0934322ef850
Song_Minhyung <song961003@gmail.com>
2023-11-21 15:49:21 +0900

    refactor: ShareButtonList 중복되는 부분 리팩토링

--- frontend/src/pages/ResultSharePage/ShareButtonList.tsx
+++ frontend/src/pages/ResultSharePage/ShareButtonList.tsx
@@ -13,49 +13,49 @@ interface ShareButtonListProps {
   cardUrl: string;
   resultSharePageRef: RefObject<HTMLDivElement>;
 }
+
 export function ShareButtonList({ cardUrl, resultSharePageRef }: ShareButtonListProps) {
   const { id } = useParams<{ id: string }>();
 
   const share = {
-    kakao: async () => {
-      await Kakao.init();
-      await Kakao.shareSendDefault({ cardUrl, id: id as string });
+    kakao: {
+      text: '카카오톡 공유',
+      icon: 'simple-icons:kakaotalk',
+      iconColor: '#FEE500',
+      onClick: async () => {
+        await Kakao.init();
+        await Kakao.shareSendDefault({ cardUrl, id: id as string });
+      },
     },
-    download: () => {
-      downloadImage(resultSharePageRef);
+    download: {
+      text: '다운로드',
+      icon: 'ic:round-download',
+      iconColor: '',
+      onClick: () => {
+        downloadImage(resultSharePageRef);
+      },
     },
-    copyLink: () => {},
+    copyLink: {
+      text: 'copyLink',
+      icon: 'bxs:copy',
+      iconColor: '',
+      onClick: () => {},
+    },
   };
 
   return (
     <ul className="w-full h-110 rounded-b-2xl flex flex-all-center gap-12 ignore-html2canvas">
-      <li>
-        <IconButton
-          text="카카오톡 공유"
-          icon="simple-icons:kakaotalk"
-          iconColor="#FEE500"
-          iconSize={RESULT_SHARE_ICON_SIZE}
-          onClick={share.kakao}
-        />
-      </li>
-      <li>
-        <IconButton
-          text="다운로드"
-          icon="ic:round-download"
-          iconColor=""
-          iconSize={RESULT_SHARE_ICON_SIZE}
-          onClick={share.download}
-        />
-      </li>
-      <li>
-        <IconButton
-          text="copyLink"
-          icon="bxs:copy"
-          iconColor=""
-          iconSize={RESULT_SHARE_ICON_SIZE}
-          onClick={share.copyLink}
-        />
-      </li>
+      {Object.entries(share).map(([key, value]) => (
+        <li key={key}>
+          <IconButton
+            text={value.text}
+            icon={value.icon}
+            iconColor={value.iconColor}
+            iconSize={RESULT_SHARE_ICON_SIZE}
+            onClick={value.onClick}
+          />
+        </li>
+      ))}
     </ul>
   );
 }
