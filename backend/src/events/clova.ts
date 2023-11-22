96c838e9bd4b18363884d55c9e8a4530dfbd7259
kimyu0218 <serena35@ewhain.net>
2023-11-22 17:39:47 +0900

    refactor: API_KEY 게이트웨이에서 파라미터로 넘기도록 수정
    Co-authored-by: HeoJiye <heoze1031@gmail.com>

--- backend/src/events/clova.ts
+++ backend/src/events/clova.ts
@@ -1,8 +1,6 @@
 import { createTransformer } from './clovaStreamTransform';
 import {
   CLOVA_URL,
-  X_NCP_APIGW_API_KEY,
-  X_NCP_CLOVASTUDIO_API_KEY,
   talkSystemMessage,
   tarotReadingSystemMessage,
 } from './constants';
@@ -16,20 +14,45 @@ export function initChatLog(): Chat[] {
   return [{ role: 'system', content: talkSystemMessage }];
 }
 
-export async function createTalk(chatLog: Chat[], message: string) {
+export async function createTalk(
+  chatLog: Chat[],
+  message: string,
+  X_NCP_APIGW_API_KEY: string,
+  X_NCP_CLOVASTUDIO_API_KEY: string,
+) {
   chatLog.push({ role: 'user', content: message });
-  return fetchClovaAPI(chatLog);
+  return fetchClovaAPI(
+    chatLog,
+    false,
+    X_NCP_APIGW_API_KEY,
+    X_NCP_CLOVASTUDIO_API_KEY,
+  );
 }
 
-export async function createTarotReading(chatLog: Chat[], tarotName: string) {
+export async function createTarotReading(
+  chatLog: Chat[],
+  tarotName: string,
+  X_NCP_APIGW_API_KEY: string,
+  X_NCP_CLOVASTUDIO_API_KEY: string,
+) {
   chatLog.push(
     { role: 'system', content: tarotReadingSystemMessage },
     { role: 'user', content: `타로 카드: ${tarotName}\n` },
   );
-  return fetchClovaAPI(chatLog, true);
+  return fetchClovaAPI(
+    chatLog,
+    true,
+    X_NCP_APIGW_API_KEY,
+    X_NCP_CLOVASTUDIO_API_KEY,
+  );
 }
 
-async function fetchClovaAPI(chatLog: Chat[], isTarotReading = false) {
+async function fetchClovaAPI(
+  chatLog: Chat[],
+  isTarotReading: boolean,
+  X_NCP_APIGW_API_KEY: string,
+  X_NCP_CLOVASTUDIO_API_KEY: string,
+) {
   const response = await fetch(CLOVA_URL, {
     method: 'POST',
     headers: {
@@ -48,6 +71,10 @@ async function fetchClovaAPI(chatLog: Chat[], isTarotReading = false) {
       topP: 0.8,
     }),
   });
+
+  if (!response.ok) {
+    throw new Error('FATCH ERROR!!!');
+  }
 
   const transformer = createTransformer();
   const messageStream = response.body?.pipeThrough(transformer);
