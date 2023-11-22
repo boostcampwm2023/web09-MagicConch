1b3d4c77cddf21de2995b296eef1ab8f021cabcf
Song_Minhyung <song961003@gmail.com>
2023-11-22 10:31:45 +0900

    design: header 컨텐츠에 따라 높이가 달라져서 고정시킴

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,12 +3,12 @@ import { LogoButton } from '@components/Buttons';
 import HeaderRight from './HeaderRight';
 
 interface HeaderProps {
-  rightItems: React.ReactNode[];
+  rightItems?: React.ReactNode[];
 }
 
 export default function Header({ rightItems }: HeaderProps) {
   return (
-    <div className="fixed top-0 w-full flex justify-between items-center surface-content text-default px-8 py-5">
+    <div className="fixed top-0 w-full h-48 flex justify-between items-center surface-content text-default px-8 py-5">
       <LogoButton />
       <HeaderRight items={rightItems} />
     </div>
