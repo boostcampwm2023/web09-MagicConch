aef3079e14da5e1e19b96a557fca2bbaa6161a9f
Song_Minhyung <song961003@gmail.com>
2023-11-22 10:32:12 +0900

    feat: ResultSharePage에 헤더 추가

--- frontend/src/pages/ResultSharePage/ResultSharePage.tsx
+++ frontend/src/pages/ResultSharePage/ResultSharePage.tsx
@@ -1,5 +1,7 @@
 import { useRef } from 'react';
 
+import Header from '@components/Header';
+
 import { getResultShareQuery } from '@stores/queries/getResultShareQuery';
 
 import { ResultImage } from './ResultImage';
@@ -16,20 +18,23 @@ function ResultSharePage({}: ResultSharePageProps) {
   const resultSharePageRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div
-      ref={resultSharePageRef}
-      className="w-screen h-full flex flex-all-center gap-80 display-medium16 surface-alt text-strong p-20"
-    >
-      <ResultImage cardUrl={card_url} />
+    <>
+      <Header />
+      <div
+        ref={resultSharePageRef}
+        className="w-screen h-full flex flex-all-center gap-80 display-medium16 surface-alt text-strong p-20"
+      >
+        <ResultImage cardUrl={card_url} />
 
-      <div className="w-664 h-640 flex flex-col rounded-2xl surface-box">
-        <ResultTextBox content={content} />
-        <ShareButtonList
-          cardUrl={card_url}
-          resultSharePageRef={resultSharePageRef}
-        />
+        <div className="w-664 h-640 flex flex-col rounded-2xl surface-box">
+          <ResultTextBox content={content} />
+          <ShareButtonList
+            cardUrl={card_url}
+            resultSharePageRef={resultSharePageRef}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
