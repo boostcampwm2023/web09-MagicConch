c09dbc7381599196b1434316c083f241b506aff3
kimyu0218 <serena35@ewhain.net>
2023-11-23 14:13:17 +0900

    fix: CORS 에러 해결

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,12 @@ import { setupSwagger } from './common/config/swagger.setting';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
