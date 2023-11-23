f6922ba18a6959bbc847d8bfbb98b02e5306b2ad
HeoJiye <heoze1031@gmail.com>
2023-11-23 15:07:43 +0900

    feat: messageBox에 tarot image 서버에서 가져오기

--- frontend/src/business/hooks/useAiChat/useTarotSpread.tsx
+++ frontend/src/business/hooks/useAiChat/useTarotSpread.tsx
@@ -5,12 +5,12 @@ import TarotSpread from '@components/TarotSpread';
 
 import { aiSocketEmit, aiSocketOn } from '@business/services/socket';
 
-export function useTarotSpread(tarotCardId: React.MutableRefObject<string | undefined>) {
+export function useTarotSpread(tarotCardId: React.MutableRefObject<number | undefined>) {
   const { open } = useOverlay();
 
   const pickCard = (idx: number) => {
     aiSocketEmit('tarotRead', idx);
-    tarotCardId.current = idx.toString().padStart(2, '0'); // TODO: server에서 가져오는 데이터로 변경되면 padStart 삭제
+    tarotCardId.current = idx;
   };
 
   const openTarotSpread = () => {
