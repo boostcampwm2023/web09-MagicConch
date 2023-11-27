fc53633d7462b81acc51f40301b31167c2a3acca
Song_Minhyung <song961003@gmail.com>
2023-11-27 16:33:01 +0900

    feat: RTCPeerConnection 관련 함수들 분리

---
+++ frontend/src/business/hooks/useRTCPeerConnection.ts
@@ -0,0 +1,38 @@
+import { useRef } from 'react';
+
+import { iceServers } from '@constants/urls';
+
+import { useSocket } from './useSocket';
+
+interface useRTCPeerConnectionProps {
+  roomName: string;
+  remoteVideoRef: React.RefObject<HTMLVideoElement | undefined>;
+}
+
+export function useRTCPeerConnection({ roomName, remoteVideoRef }: useRTCPeerConnectionProps) {
+  const peerConnectionRef = useRef<RTCPeerConnection>();
+
+  const { socketEmit } = useSocket('WebRTC');
+
+  const makeConnection = () => {
+    peerConnectionRef.current = new RTCPeerConnection({ iceServers: [{ urls: iceServers }] });
+
+    peerConnectionRef.current.addEventListener('track', e => {
+      if (!remoteVideoRef.current) {
+        return;
+      }
+
+      remoteVideoRef.current.srcObject = e.streams[0];
+    });
+
+    peerConnectionRef.current.addEventListener('icecandidate', e => {
+      if (!e.candidate) {
+        return;
+      }
+
+      socketEmit('candidate', e.candidate, roomName);
+    });
+  };
+
+  return { makeConnection, peerConnectionRef };
+}
