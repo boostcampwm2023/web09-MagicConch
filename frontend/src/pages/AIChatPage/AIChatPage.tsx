5202cc0a10e9e96f44fa1e0aa7b11f41ec02503b
HeoJiye <heoze1031@gmail.com>
2023-11-28 17:00:45 +0900

    remove: 안쓰는 import 삭제

--- frontend/src/pages/AIChatPage/AIChatPage.tsx
+++ frontend/src/pages/AIChatPage/AIChatPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import Background from '@components/Background';
 import ChatContainer from '@components/ChatContainer';
