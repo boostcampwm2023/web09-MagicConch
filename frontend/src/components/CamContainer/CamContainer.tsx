7c05668b46691ca2d88d3d648d232589968fd051
HeoJiye <heoze1031@gmail.com>
2023-11-29 17:53:17 +0900

    feat: 타로 카드 펼치기 버튼 추가

--- frontend/src/components/CamContainer/CamContainer.tsx
+++ frontend/src/components/CamContainer/CamContainer.tsx
@@ -1,5 +1,7 @@
-import { IconToggleButton } from '@components/Buttons';
+import { CustomButton, IconToggleButton } from '@components/Buttons';
 import CamBox from '@components/CamBox';
+
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 interface CamContainerProps {
   localVideoRef: React.RefObject<HTMLVideoElement>;
@@ -34,23 +36,33 @@ export default function CamContainer({
           defaultImage="bg-sponge"
         />
       </div>
-      <div className="flex-with-center gap-48">
-        <IconToggleButton
-          activeIcon="pepicons-pop:camera"
-          disabledIcon="pepicons-pop:camera-off"
-          iconSize={28}
-          buttonSize="l"
-          active={cameraConnected.local}
-          onClick={toggleVideo}
-        />
-        <IconToggleButton
-          activeIcon="mingcute:mic-line"
-          disabledIcon="mingcute:mic-off-line"
-          iconSize={28}
-          buttonSize="l"
-          active={cameraConnected.local}
-          onClick={toggleAudio}
-        />
+      <div className="flex flex-col gap-30">
+        <CustomButton size="m">
+          <Icon
+            width="17"
+            height="17"
+            icon="tabler:cards-filled"
+          />
+          타로 카드 펼치기
+        </CustomButton>
+        <div className="flex-with-center gap-48">
+          <IconToggleButton
+            activeIcon="pepicons-pop:camera"
+            disabledIcon="pepicons-pop:camera-off"
+            iconSize={28}
+            buttonSize="l"
+            active={cameraConnected.local}
+            onClick={toggleVideo}
+          />
+          <IconToggleButton
+            activeIcon="mingcute:mic-line"
+            disabledIcon="mingcute:mic-off-line"
+            iconSize={28}
+            buttonSize="l"
+            active={cameraConnected.local}
+            onClick={toggleAudio}
+          />
+        </div>
       </div>
     </div>
   );
