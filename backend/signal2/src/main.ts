2b353e4f61fed07ea2cc14c1be13163767da18d5
kimyu0218 <serena35@ewhain.net>
2023-11-30 11:33:21 +0900

    feat: 최상위 모듈 및 main 작성
    - EventsModule, LoggerModule 가져오기
    - 추후 동적으로 포트번호 설정하도록 수정 필요
    
    Co-authored-by: Doosies <song961003@gmail.com>

---
+++ backend/signal2/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { LoggerService } from './logger/logger.service';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(LoggerService));
+
+  /**
+   * TODO : 추후 동적으로 포트번호 설정하도록 수정
+   */
+  await app.listen(3001);
+}
+bootstrap();
