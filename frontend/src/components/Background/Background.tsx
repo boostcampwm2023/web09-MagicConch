c1a28c6aeb14844d20ea4955dffe0cf2ee695a17
iQuQi <kidscop99@naver.com>
2023-12-02 23:15:15 +0900

    feat: 테마 토글 추가 및 디자인 일부 보완

--- frontend/src/components/Background/Background.tsx
+++ frontend/src/components/Background/Background.tsx
@@ -1,26 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+
 interface BackgroundProps {
   children?: React.ReactNode;
   type?: 'default' | 'open' | 'close' | 'dynamic';
 }
 
 function Background({ children, type = 'default' }: BackgroundProps) {
-  const fadeInIfOpen = type == 'open' ? 'animate-fadeIn' : '';
-  const fadeOutIfClose = type == 'close' ? 'animate-fadeOut' : '';
-  const fadeInIfDynamic = type == 'dynamic' ? 'animate-fadeIn' : '';
+  const html = document.querySelector('html');
+  const [conchAnimation, setConchAnimation] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(html?.dataset.theme === 'dark');
+  const [initMode, setInitMode] = useState<boolean>(true);
+
+  const fadeInIfOpen = type === 'open' ? 'animate-fadeIn' : '';
+  const fadeOutIfClose = type === 'close' ? 'animate-fadeOut' : '';
+  const fadeInIfDynamic = type === 'dynamic' ? 'animate-fadeIn' : '';
+
+  useEffect(() => {
+    addEventListener('animationend', removeAnimation);
+    return () => removeEventListener('animationend', removeAnimation);
+  }, []);
+
+  const removeAnimation = ({ animationName }: AnimationEvent) => {
+    if (animationName === 'rotatingConch') setConchAnimation(false);
+  };
+
+  const toggleTheme = () => {
+    if (!html) return;
+    if (initMode) setInitMode(false);
+
+    const theme = html.dataset.theme;
+    html.dataset.theme = theme === 'dark' ? 'light' : 'dark';
+    setDarkMode(!darkMode);
+    setConchAnimation(true);
+  };
 
   return (
     <div className={`w-h-screen flex flex-with-center  ${fadeInIfOpen} ${fadeOutIfClose}`}>
-      <div className="w-h-screen absolute -z-10 flex flex-col flex-with-center ">
+      <div className="w-h-screen absolute flex flex-col flex-with-center">
         <img
-          className="absolute w-h-screen object-cover -z-10"
-          src="/bg.png"
-          alt="밤 하늘의 배경 이미지"
+          className={`absolute w-h-screen object-cover
+          ${!initMode && (darkMode ? 'animate-fadeOut' : 'animate-fadeIn')}`}
+          src="/bg-light.png"
+          alt="낮하늘 배경 이미지"
         />
         <img
-          className="absolute w-285 h-285 -z-10 animate-shining top-150"
-          src="/moon.png"
-          alt="빛나는 마법의 소라 고둥"
+          className={`absolute w-h-screen object-cover ${initMode && !darkMode && 'hidden'} 
+          ${!initMode && (darkMode ? 'animate-fadeIn' : 'animate-fadeOut')}`}
+          src="/bg-night.png"
+          alt="밤하늘 배경 이미지"
         />
+
+        <button
+          className={`absolute -bottom-[20%] -translate-y-[65vh] hover:scale-105 
+          ${conchAnimation && 'animate-rotatingConch'}`}
+          onClick={toggleTheme}
+        >
+          <img
+            className="animate-shining w-[35vh] h-[35vh] "
+            src="/moon.png"
+            alt="빛나는 마법의 소라 고둥"
+          />
+        </button>
         {type != 'default' && <div className={`absolute w-h-screen bg-black/75 ${fadeInIfDynamic}`} />}
       </div>
 
