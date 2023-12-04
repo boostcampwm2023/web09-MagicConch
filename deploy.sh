069920f64925c07b376291f96be94657a26b85fc
kimyu0218 <serena35@ewhain.net>
2023-12-05 03:51:33 +0900

    fix: 종료할 컨테이너가 없어서 발생하는 깃헙 액션 에러 핸들링

--- deploy.sh
+++ deploy.sh
@@ -20,20 +20,14 @@ fi
 
 DOCKER_COMPOSE_FILE="docker-compose.$GITHUB_SHA.$RUN_TARGET.yml"
 
-echo ">>> Start image building... $DOCKER_COMPOSE_FILE" > $DEBUG_LOG
-
 docker-compose -f "$DOCKER_COMPOSE_FILE" pull
 docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
-
-echo "<<< Build Complete..." >> $DEBUG_LOG
 
 reload_application() {
   local CONTAINER_NAME="$1"
   local RUN_PORT="$2"
   local STOP_PORT="$3"
   local CMD="$4"
-
-  echo ">>> Start $CONTAINER_NAME reloading..." >> $DEBUG_LOG
 
   CONTAINER_ID=$(docker ps --filter "name=$CONTAINER_NAME" -q)
 
@@ -42,15 +36,10 @@ reload_application() {
 
   docker exec $CONTAINER_ID /bin/bash -c "sed -i 's/port: number = $STOP_PORT/port: number = $RUN_PORT/' $MAIN_SCRIPT"
   docker exec $CONTAINER_ID /bin/bash -c "$CMD"
-
-  echo "<<< Reload Complete..." >> $DEBUG_LOG
 }
 
 reload_application "was-$RUN_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT "$NPM_BUILD && $NPM_PROD"
 reload_application "signal-$RUN_TARGET" $((WAS_RUN_PORT + 1)) $((WAS_STOP_PORT + 1)) "$NPM_BUILD && $NPM_PROD"
-
-
-echo ">>> Start nginx reloading..." >> debug.log
 
 NGINX_ID=$(docker ps --filter "name=nginx" -q)
 NGINX_CONFIG="/etc/nginx/conf.d/default.conf"
@@ -59,13 +48,14 @@ docker exec $NGINX_ID /bin/bash -c "sed -i 's/was-$STOP_TARGET:$WAS_STOP_PORT/wa
 docker exec $NGINX_ID /bin/bash -c "sed -i 's/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/' $NGINX_CONFIG"
 docker exec $NGINX_ID nginx -s reload
 
-echo "<<< Reload Complete..." >> debug.log
-
-
 while [ -z "$(docker ps --filter "name=was-$RUN_TARGET" --quiet)" ]; do
   sleep 3
 done
 
 sleep 60
 rm .env
-docker rm -f $(docker ps --filter "name=$STOP_TARGET" --quiet)
+
+STOP_CONTAINER_ID=$(docker ps --filter "name=$STOP_TARGET" --quiet)
+if [ -n "$STOP_CONTAINER_ID" ]; then
+  docker rm -f $STOP_CONTAINER_ID
+fi
