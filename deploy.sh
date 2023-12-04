5303760b69a77104fa9e5a7425e51f78797f409d
kimyu0218 <serena35@ewhain.net>
2023-12-05 02:25:33 +0900

    feat: 포트번호 수정 로직 및 빌드/실행 추가

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 GITHUB_SHA=$1
+MAIN_SCRIPT="src/main.ts"
+NPM_BUILD="npm run build"
+NPM_PROD="npm run start:prod"
 
 if docker ps --filter "name=was-blue" --format '{{.ID}}' | grep -E .; then
   RUN_TARGET="green"
   STOP_TARGET="blue"
   WAS_RUN_PORT=3002
   WAS_STOP_PORT=3000
-  echo "WAS_PORT=3002" >> .env
 else
   RUN_TARGET="blue"
   STOP_TARGET="green"
@@ -21,6 +23,28 @@ docker-compose -f "docker-compose.$GITHUB_SHA.$RUN_TARGET.yml" pull
 docker-compose -f "docker-compose.$GITHUB_SHA.$RUN_TARGET.yml" up -d
 
 echo "<<< Build Complete..." >> debug.log
+
+
+echo ">>> Start was reloading..." >> debug.log
+
+WAS_ID=$(docker ps --filter "name=was-$RUN_TARGET" -q)
+
+docker exec $WAS_ID /bin/bash -c "sed -i 's/port: number = $WAS_STOP_PORT/port: number = $WAS_RUN_PORT/' $MAIN_SCRIPT"
+docker exec $WAS_ID /bin/bash -c "$NPM_BUILD"
+docker exec $WAS_ID /bin/bash -c "$NPM_PROD"
+
+echo "<<< Reload Complete..." >> debug.log
+
+
+echo ">>> Start signal reloading..." >> debug.log
+
+SIGNAL_ID=$(docker ps --filter "name=signal-$RUN_TARGET" -q)
+
+docker exec $SIGNAL_ID /bin/bash -c "sed -i 's/port: number = $((WAS_STOP_PORT + 1))/port: number = $((WAS_RUN_PORT + 1))/' $MAIN_SCRIPT"
+docker exec $SIGNAL_ID /bin/bash -c "$NPM_BUILD"
+docker exec $SIGNAL_ID /bin/bash -c "$NPM_PROD"
+
+echo "<<< Reload Complete..." >> debug.log
 
 
 echo ">>> Start nginx reloading..." >> debug.log
