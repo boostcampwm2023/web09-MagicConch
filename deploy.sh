ee02d5d5b330cfb66fe36eb32ae2a59ed3a261a6
kimyu0218 <serena35@ewhain.net>
2023-12-04 16:11:20 +0900

    fix: 환경변수 오류 수정

--- deploy.sh
+++ deploy.sh
@@ -14,21 +14,21 @@ else
 fi
 
 echo "Start building... >> RUN $RUN_TARGET / STOP $STOP_TARGET" > debug.log
-docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" pull
-docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" up -d
+docker-compose -f "docker-compose.${{ github.sha }}.$RUN_TARGET.yml" pull
+docker-compose -f "docker-compose.${{ github.sha }}.$RUN_TARGET.yml" up -d
 echo "Build Complete..." >> debug.log
 
 echo "Start reloading... >> RUN $RUN_TARGET / STOP $STOP_TARGET" >> debug.log
-sed -i "s/was-${STOP_TARGET}:${WAS_STOP_PORT}/was-${RUN_TARGET}:${WAS_RUN_PORT}/" config/nginx/default.conf
-sed -i "s/signal-${STOP_TARGET}:${WAS_STOP_PORT + 1}/signal-${RUN_TARGET}:${WAS_RUN_PORT + 1}/" config/nginx/default.conf
-docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" exec nginx nginx -s reload
+sed -i "s/was-$STOP_TARGET:$WAS_STOP_PORT/was-$RUN_TARGET:$WAS_RUN_PORT/" config/nginx/default.conf
+sed -i "s/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/" config/nginx/default.conf
+docker-compose -f "docker-compose.${{ github.sha }}.$RUN_TARGET.yml" exec nginx nginx -s reload
 echo "Reload Complete..." >> debug.log
 
-while [ -z "$(docker ps --filter "name=was-${RUN_TARGET}" --quiet)" ]; do
+while [ -z "$(docker ps --filter "name=was-$RUN_TARGET" --quiet)" ]; do
   sleep 3
 done
 sleep 60
 
 
 rm .env
-docker rm -f $(docker ps --filter "name=${STOP_TARGET}" --quiet)
+docker rm -f $(docker ps --filter "name=$STOP_TARGET" --quiet)
