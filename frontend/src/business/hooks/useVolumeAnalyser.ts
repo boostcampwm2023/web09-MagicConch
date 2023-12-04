1982db1e533d8a9c6b5cdb88d8b588f498dff04d
HeoJiye <heoze1031@gmail.com>
2023-12-04 11:21:40 +0900

    fix: video element에서 stream 가져오는 방식 수정
    - 기존 방식은 srcObject였는데 null로 인식되서 오류가 날 때도 있음.
    - captureStream은 대신에 옛날 브라우저에서는 동작하지 않을 수 있음

--- frontend/src/business/hooks/useVolumeAnalyser.ts
+++ frontend/src/business/hooks/useVolumeAnalyser.ts
@@ -2,50 +2,49 @@ import { useEffect } from 'react';
 
 export default function useVolumeAnalyser(videoRef: React.RefObject<HTMLVideoElement>) {
   useEffect(() => {
-    if (!videoRef.current) {
+    if (!videoRef.current || !('captureStream' in HTMLVideoElement.prototype)) {
       return;
     }
+    const videoElement = videoRef.current as any;
 
     const audioContext = new window.AudioContext();
-    const stream = videoRef.current.srcObject as MediaStream;
+    const stream = videoElement.captureStream();
 
-    const source = audioContext.createMediaStreamSource(stream);
-    const analyser = audioContext.createAnalyser();
+    stream.onactive = () => {
+      const source = audioContext.createMediaStreamSource(stream);
+      const analyser = audioContext.createAnalyser();
 
-    source.connect(analyser);
+      source.connect(analyser);
 
-    analyser.fftSize = 256;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
+      analyser.fftSize = 256;
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
 
-    let prevVolume = 0;
+      let prevVolume = 0;
 
-    setInterval(() => {
-      analyser.getByteFrequencyData(dataArray);
+      setInterval(() => {
+        analyser.getByteFrequencyData(dataArray);
 
-      const averageVolume = calculateAverageVolume(dataArray);
+        const averageVolume = calculateAverageVolume(dataArray);
 
-      videoRef.current!.animate(
-        [
-          { filter: `drop-shadow(0px 0px ${prevVolume}px #0052F0` },
-          { filter: `drop-shadow(0px 0px ${averageVolume}px #0052F0` },
-        ],
-        {
-          duration: 200,
-          fill: 'forwards',
-        },
-      );
-      prevVolume = averageVolume;
-      console.log('Average Volume:', averageVolume);
-    }, 500);
+        videoRef.current!.animate(
+          [
+            { filter: `drop-shadow(0px 0px ${prevVolume}px #0052F0` },
+            { filter: `drop-shadow(0px 0px ${averageVolume}px #0052F0` },
+          ],
+          {
+            duration: 200,
+            fill: 'forwards',
+          },
+        );
+        prevVolume = averageVolume;
+        console.log('Average Volume:', averageVolume);
+      }, 500);
+    };
 
     function calculateAverageVolume(array: Uint8Array) {
       const sum = array.reduce((acc, value) => acc + value, 0);
-
-      const max = Math.max(sum / array.length - 30, 0);
-      const result = Math.min(max, 70);
-
-      return Math.floor(result);
+      return Math.floor(Math.min(sum, 70));
     }
   }, [videoRef.current]);
 }
