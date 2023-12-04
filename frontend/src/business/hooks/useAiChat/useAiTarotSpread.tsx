ba73280cdebe07aadeadd987eeccab771c82f5ad
HeoJiye <heoze1031@gmail.com>
2023-12-04 16:29:36 +0900

    feat: useAiTarotSpread에서도 socketManager 사용하도록 수정

--- frontend/src/business/hooks/useAiChat/useAiTarotSpread.tsx
+++ frontend/src/business/hooks/useAiChat/useAiTarotSpread.tsx
@@ -1,18 +1,19 @@
-import { useSocket } from '../useSocket';
 import { useTarotSpread } from '../useTarotSpread';
 import { useEffect } from 'react';
 
+import { AISocketManager } from '@business/services/SocketManager';
+
 export function useAiTarotSpread(setTarotId: (idx: number) => void) {
-  const { socketEmit, socketOn } = useSocket('AIChat');
+  const socketManager = new AISocketManager();
 
   const pickCard = (idx: number) => {
-    socketEmit('tarotRead', idx);
+    socketManager.emit('tarotRead', idx);
     setTarotId(idx);
   };
 
   const { openTarotSpread } = useTarotSpread(pickCard);
 
   useEffect(() => {
-    socketOn('tarotCard', () => setTimeout(openTarotSpread, 1000));
+    socketManager.on('tarotCard', () => setTimeout(openTarotSpread, 1000));
   }, []);
 }
