1cde9342db0cfe25ca801c4b4c26b793e145eb8d
kimyu0218 <serena35@ewhain.net>
2023-12-04 16:02:33 +0900

    test: 디버깅 코드 추가

--- deploy.sh
+++ deploy.sh
@@ -13,19 +13,22 @@ else
   WAS_STOP_PORT=3002
 fi
 
-echo "Start building... >> RUN $RUN_TARGET / STOP $STOP_TARGET" >> build.debug
+echo "Start building... >> RUN $RUN_TARGET / STOP $STOP_TARGET" > debug.log
 docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" pull
 docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" up -d
+echo "Build Complete..." >> debug.log
 
+echo "Start reloading... >> RUN $RUN_TARGET / STOP $STOP_TARGET" >> debug.log
 sed -i "s/was-${STOP_TARGET}:${WAS_STOP_PORT}/was-${RUN_TARGET}:${WAS_RUN_PORT}/" config/nginx/default.conf
 sed -i "s/signal-${STOP_TARGET}:${WAS_STOP_PORT + 1}/signal-${RUN_TARGET}:${WAS_RUN_PORT + 1}/" config/nginx/default.conf
 docker-compose -f "docker-compose.${{ github.sha }}.${RUN_TARGET}.yml" exec nginx nginx -s reload
+echo "Reload Complete..." >> debug.log
 
 while [ -z "$(docker ps --filter "name=was-${RUN_TARGET}" --quiet)" ]; do
   sleep 3
 done
 sleep 60
 
-echo "delete old version..."
+
 rm .env
 docker rm -f $(docker ps --filter "name=${STOP_TARGET}" --quiet)
