926c6e7e4d556ed9ef47717c15da999a273cf52a
kimyu0218 <serena35@ewhain.net>
2023-12-05 16:32:13 +0900

    fix: 깃헙 액션 타임아웃 늘리기 & 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -14,32 +14,15 @@ change_port() {
   local CONTAINER_ID="$1"
   local RUN_PORT="$2"
   local STOP_PORT="$3"
-  
-  #NODE_PROCESS=$(docker exec $CONTAINER_ID pgrep -f 'npm run start')
 
-  #if [ -n "$NODE_PROCESS" ]; then
-  #  echo "kill PID #$NODE_PROCESS..." >> $DEBUG_LOG
-  #  docker exec $CONTAINER_ID kill $NODE_PROCESS
-  #  sleep 10
-  #fi
-
-  echo "kill process running on $STOP_PORT..." >> $DEBUG_LOG
+  echo "kill process running on $STOP_PORT ..." >> $DEBUG_LOG
   docker exec $CONTAINER_ID /bin/bash -c "pkill -f ':$STOP_PORT'"
     
-  echo "change port... ($STOP_PORT to $RUN_PORT)" >> $DEBUG_LOG
+  echo "change port ... ($STOP_PORT to $RUN_PORT)" >> $DEBUG_LOG
   docker exec $CONTAINER_ID /bin/bash -c "sed -i 's/port: number = $STOP_PORT/port: number = $RUN_PORT/' $MAIN_SCRIPT"
 
   echo "restart application ..." >> $DEBUG_LOG
-  docker exec $CONTAINER_ID /bin/bash -c "$NPM_BUILD"
-  echo "build success..." >> $DEBUG_LOG
-  docker exec $CONTAINER_ID /bin/bash -c "$NPM_PROD"
-  echo "run success..." >> $DEBUG_LOG
-
-  CMD_EXIT_CODE=$?
-  if [ $CMD_EXIT_CODE -ne 0 ]; then
-    echo "Error: Failed to restart application. Exit code: $CMD_EXIT_CODE" >> $DEBUG_LOG
-    exit 1
-  fi
+  docker exec -d $CONTAINER_ID /bin/bash -c "$NPM_BUILD && $NPM_PROD"
 }
 
 reload_application() {
@@ -52,11 +35,54 @@ reload_application() {
 
   if ((RUN_PORT > 3001)); then
     change_port "$CONTAINER_ID" $RUN_PORT $STOP_PORT
-    echo ">>> Reload complete... $CONTAINER_NAME running on $RUN_PORT" >> $DEBUG_LOG
+    echo ">>> Reload complete ... $CONTAINER_NAME running on $RUN_PORT" >> $DEBUG_LOG
   else
-    echo ">>> Reload pass... $CONTAINER_NAME running on $RUN_PORT" >> $DEBUG_LOG
+    echo ">>> Reload pass ... $CONTAINER_NAME running on $RUN_PORT" >> $DEBUG_LOG
   fi
   print_line
+}
+
+reload_nginx() {
+  local RUN_TARGET="$1"
+  local STOP_TARGET="$2"
+  local WAS_RUN_PORT="$3"
+  local WAS_STOP_PORT="$4"
+
+  echo "<<< Reload nginx ..." >> $DEBUG_LOG
+
+  NGINX_ID=$(docker ps --filter "name=nginx" -q)
+  NGINX_CONFIG="/etc/nginx/conf.d/default.conf"
+
+  docker exec $NGINX_ID /bin/bash -c "sed -i 's/was-$STOP_TARGET:$WAS_STOP_PORT/was-$RUN_TARGET:$WAS_RUN_PORT/' $NGINX_CONFIG"
+  docker exec $NGINX_ID /bin/bash -c "sed -i 's/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/' $NGINX_CONFIG"
+  docker exec $NGINX_ID /bin/bash -c "nginx -s reload"
+
+  echo ">>> Reload complete..." >> $DEBUG_LOG
+  print_line
+}
+
+deploy() {
+  local RUN_TARGET="$1"
+  local STOP_TARGET="$2"
+  local WAS_RUN_PORT="$3"
+  local WAS_STOP_PORT="$4"
+  
+  reload_application "was-$RUN_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT
+  reload_application "signal-$RUN_TARGET" $((WAS_RUN_PORT + 1)) $((WAS_STOP_PORT + 1))
+
+  wait 
+
+  reload_nginx "$RUN_TARGET" "$STOP_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT
+
+  echo "Delete .env file ..." >> $DEBUG_LOG
+  print_line
+  rm .env
+
+  echo "Down old version ..." >> $DEBUG_LOG
+  STOP_CONTAINER_ID=$(docker ps --filter "name=$STOP_TARGET" --quiet)
+  if [ -n "$STOP_CONTAINER_ID" ]; then
+    docker rm -f $STOP_CONTAINER_ID
+  fi
 }
 
 if docker ps --filter "name=was-blue" --format '{{.ID}}' | grep -E .; then
@@ -73,37 +99,12 @@ fi
 
 DOCKER_COMPOSE_FILE="docker-compose.$GITHUB_SHA.$RUN_TARGET.yml"
 
-echo "<<< Run docker compose... $DOCKER_COMPOSE_FILE" > $DEBUG_LOG
+echo "<<< Run docker compose ... $DOCKER_COMPOSE_FILE" > $DEBUG_LOG
 
 docker-compose -f "$DOCKER_COMPOSE_FILE" pull
 docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
 
-echo ">>> Run complete..." >> $DEBUG_LOG
+echo ">>> Run complete ..." >> $DEBUG_LOG
 print_line
 
-reload_application "was-$RUN_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT
-reload_application "signal-$RUN_TARGET" $((WAS_RUN_PORT + 1)) $((WAS_STOP_PORT + 1))
-
-echo "<<< Reload nginx..." >> $DEBUG_LOG
-
-NGINX_ID=$(docker ps --filter "name=nginx" -q)
-NGINX_CONFIG="/etc/nginx/conf.d/default.conf"
-
-docker exec $NGINX_ID /bin/bash -c "sed -i 's/was-$STOP_TARGET:$WAS_STOP_PORT/was-$RUN_TARGET:$WAS_RUN_PORT/' $NGINX_CONFIG"
-docker exec $NGINX_ID /bin/bash -c "sed -i 's/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/' $NGINX_CONFIG"
-docker exec $NGINX_ID /bin/bash -c "nginx -s reload"
-
-echo ">>> Reload complete..." >> $DEBUG_LOG
-print_line
-
-sleep 30
-
-echo "Delete .env file..." >> $DEBUG_LOG
-print_line
-rm .env
-
-echo "Down old version..." >> $DEBUG_LOG
-STOP_CONTAINER_ID=$(docker ps --filter "name=$STOP_TARGET" --quiet)
-if [ -n "$STOP_CONTAINER_ID" ]; then
-  docker rm -f $STOP_CONTAINER_ID
-fi
+deploy "$RUN_TARGET" "$STOP_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT
