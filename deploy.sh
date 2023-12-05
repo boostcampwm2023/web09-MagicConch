94fdeb6b17c35130b1ab9d3d5e0746fa4e37a559
kimyu0218 <serena35@ewhain.net>
2023-12-05 10:53:54 +0900

    test: 디버깅용 echo문 추가

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,31 @@ DEBUG_LOG="debug.log"
 NPM_BUILD="npm run build"
 NPM_PROD="npm run start:prod"
 
+print_line() {
+  echo " " >> $DEBUG_LOG
+}
+
+reload_application() {
+  local CONTAINER_NAME="$1"
+  local RUN_PORT="$2"
+  local STOP_PORT="$3"
+  local CMD="$4"
+
+  echo "<<< Start reload $CONTAINER_NAME... ($RUN_PORT)" >> $DEBUG_LOG
+
+  CONTAINER_ID=$(docker ps --filter "name=$CONTAINER_NAME" -q)
+
+  PID=$(docker exec $CONTAINER_ID /bin/bash -c "lsof -t -i:$RUN_PORT")
+  echo "kill $PID..." >> $DEBUG_LOG
+  docker exec $CONTAINER_ID /bin/bash -c "kill -9 $PID"
+
+  docker exec $CONTAINER_ID /bin/bash -c "sed -i 's/port: number = $STOP_PORT/port: number = $RUN_PORT/' $MAIN_SCRIPT"
+  docker exec $CONTAINER_ID /bin/bash -c "$CMD"
+
+  echo ">>> Reload complete... $CONTAINER_NAME running on $RUN_PORT" >> $DEBUG_LOG
+  print_line
+}
+
 if docker ps --filter "name=was-blue" --format '{{.ID}}' | grep -E .; then
   RUN_TARGET="green"
   STOP_TARGET="blue"
@@ -25,26 +50,8 @@ echo "<<< Start docker compose building... $DOCKER_COMPOSE_FILE" > $DEBUG_LOG
 docker-compose -f "$DOCKER_COMPOSE_FILE" pull
 docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
 
-echo ">>> Build complete...\n" >> $DEBUG_LOG
-
-reload_application() {
-  local CONTAINER_NAME="$1"
-  local RUN_PORT="$2"
-  local STOP_PORT="$3"
-  local CMD="$4"
-
-  echo "<<< Start reload $CONTAINER_NAME..." >> $DEBUG_LOG
-
-  CONTAINER_ID=$(docker ps --filter "name=$CONTAINER_NAME" -q)
-
-  PID=$(docker exec $CONTAINER_ID /bin/bash -c "lsof -t -i:$RUN_PORT")
-  docker exec $CONTAINER_ID /bin/bash -c "kill -9 $PID"
-
-  docker exec $CONTAINER_ID /bin/bash -c "sed -i 's/port: number = $STOP_PORT/port: number = $RUN_PORT/' $MAIN_SCRIPT"
-  docker exec $CONTAINER_ID /bin/bash -c "$CMD"
-
-  echo ">>> Reload complete... $CONTAINER_NAME running ong $RUN_PORT\n" >> $DEBUG_LOG
-}
+echo ">>> Build complete..." >> $DEBUG_LOG
+print_line
 
 reload_application "was-$RUN_TARGET" $WAS_RUN_PORT $WAS_STOP_PORT "$NPM_BUILD && $NPM_PROD"
 reload_application "signal-$RUN_TARGET" $((WAS_RUN_PORT + 1)) $((WAS_STOP_PORT + 1)) "$NPM_BUILD && $NPM_PROD"
@@ -58,15 +65,16 @@ docker exec $NGINX_ID /bin/bash -c "sed -i 's/was-$STOP_TARGET:$WAS_STOP_PORT/wa
 docker exec $NGINX_ID /bin/bash -c "sed -i 's/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/' $NGINX_CONFIG"
 docker exec $NGINX_ID /bin/bash -c "nginx -s reload"
 
-
-echo ">>> Reload complete...\n" >> $DEBUG_LOG
+echo ">>> Reload complete..." >> $DEBUG_LOG
+print_line
 
 sleep 60
 
-echo "Delete .env file...\n" >> $DEBUG_LOG
+echo "Delete .env file..." >> $DEBUG_LOG
+print_line
 rm .env
 
-echo "Down old version...\n" >> $DEBUG_LOG
+echo "Down old version..." >> $DEBUG_LOG
 STOP_CONTAINER_ID=$(docker ps --filter "name=$STOP_TARGET" --quiet)
 if [ -n "$STOP_CONTAINER_ID" ]; then
   docker rm -f $STOP_CONTAINER_ID
