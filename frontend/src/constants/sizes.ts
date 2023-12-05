2fa3cbda5c5efb05af9637ef145ca6c6c30e30a8
HeoJiye <heoze1031@gmail.com>
2023-12-05 16:23:59 +0900

    fix: size 재조정 + 맥 환경에 따른 leading 설정 다시 추가

--- frontend/src/constants/sizes.ts
+++ frontend/src/constants/sizes.ts
@@ -1,13 +1,17 @@
-export const RESULT_SHARE_ICON_SIZE = 20;
+import { detect } from 'detect-browser';
 
+export const RESULT_SHARE_ICON_SIZE = 20;
 export const TAROT_CARDS_LENGTH = 78;
 
 export type ButtonSize = 's' | 'm' | 'l';
 
+const browser = detect();
+const __MAC__ = browser?.os?.includes('Mac');
+
 export const buttonSizeMap: Record<ButtonSize, string> = {
-  s: `display-bold14 p-8`,
-  m: `display-bold16 p-8`,
-  l: `display-bold24 p-12`,
+  s: `display-bold14 p-8 ${__MAC__ ? 'leading-18' : ''}`,
+  m: `display-bold16 p-12 ${__MAC__ ? 'leading-20' : ''}`,
+  l: `display-bold24 p-12 ${__MAC__ ? 'leading-30' : ''}`,
 };
 
 export const iconSizeMap: Record<ButtonSize, string> = {
