2775a1e88f90ad6593fe962c56fa5f655f3d9fd5
Song_Minhyung <song961003@gmail.com>
2023-12-06 19:23:04 +0900

    remove: chatPageState roomName 제거
    - 사용하지 않음

--- frontend/src/pages/HumanChatPage/useHumanChatPageCreateRoomEvent.ts
+++ frontend/src/pages/HumanChatPage/useHumanChatPageCreateRoomEvent.ts
@@ -3,13 +3,16 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 import { HumanSocketManager } from '@business/services/SocketManager';
 
+export interface ChatPageState {
+  joined: boolean;
+  host: boolean;
+}
 export function useHumanChatPageCreateRoomEvent() {
   const humanSocket = new HumanSocketManager();
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [chatPageState, setChatPageState] = useState({
-    roomName: '',
+  const [chatPageState, setChatPageState] = useState<ChatPageState>({
     joined: false,
     host: false,
   });
