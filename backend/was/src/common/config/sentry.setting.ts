5d1db2d66cdc7bd3ef9ac7a2d08c876cd2a88662
kimyu0218 <serena35@ewhain.net>
2023-12-07 14:21:45 +0900

    feat: 어플리케이션 정보 Sentry로 전달
    - errorHandler() : 예외 추적
    - tracingHandler() : 코드 성능 추적
    - requestHandler() : HTTP 요청 추적

--- backend/was/src/common/config/sentry.setting.ts
+++ backend/was/src/common/config/sentry.setting.ts
@@ -1,11 +1,16 @@
+import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
 import * as Sentry from '@sentry/node';
 import { ProfilingIntegration } from '@sentry/profiling-node';
 
-export function setupSentry(dsn: string): void {
+export function setupSentry(app: INestApplication, dsn: string): void {
   Sentry.init({
     dsn: dsn,
     integrations: [new ProfilingIntegration()],
     tracesSampleRate: 1.0,
     profilesSampleRate: 1.0,
   });
+
+  app.use(Sentry.Handlers.errorHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+  app.use(Sentry.Handlers.requestHandler());
 }
