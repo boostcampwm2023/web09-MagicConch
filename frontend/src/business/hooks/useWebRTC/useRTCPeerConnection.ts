05dfa3247fb2bc379ecf0cf6c4623f3b19d6b578
Song_Minhyung <song961003@gmail.com>
2023-12-07 12:01:21 +0900

    feat: iceserver - dev, prod 분리

--- frontend/src/business/hooks/useWebRTC/useRTCPeerConnection.ts
+++ frontend/src/business/hooks/useWebRTC/useRTCPeerConnection.ts
@@ -2,6 +2,8 @@ import { useEffect, useRef } from 'react';
 
 import { HumanSocketManager } from '@business/services/SocketManager';
 
+import { iceServers } from '@constants/urls';
+
 interface useRTCPeerConnectionParams {
   remoteVideoRef: React.RefObject<HTMLVideoElement | undefined>;
 }
@@ -12,15 +14,18 @@ export function useRTCPeerConnection({ remoteVideoRef }: useRTCPeerConnectionPar
 
   const socketManager = new HumanSocketManager();
 
+  const prodIceServerConfig = [
+    {
+      urls: `${import.meta.env.VITE_WAS_URL}/turn`,
+      credential: import.meta.env.VITE_ICE_SERVER_CREDENTIAL,
+      username: import.meta.env.VITE_ICER_SERVER_USERNAME,
+    },
+  ];
+  const devIceServerConfig = [{ urls: iceServers }];
+
   const makeRTCPeerConnection = async ({ roomName }: { roomName: string }) => {
     peerConnectionRef.current = new RTCPeerConnection({
-      iceServers: [
-        {
-          urls: `${import.meta.env.VITE_WAS_URL}/turn`,
-          credential: import.meta.env.VITE_ICE_SERVER_CREDENTIAL,
-          username: import.meta.env.VITE_ICER_SERVER_USERNAME,
-        },
-      ],
+      iceServers: import.meta.env.MODE === 'development' ? devIceServerConfig : prodIceServerConfig,
     });
 
     peerConnectionRef.current.addEventListener('track', e => {
