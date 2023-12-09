8ede6b6c7e03ca3baaf3a4c25eb73dc1e217b3b9
kimyu0218 <serena35@ewhain.net>
2023-12-09 17:27:13 +0900

    fix: winston transport 타입 오류 수정

--- backend/was/src/logger/winston/winston.transports.ts
+++ backend/was/src/logger/winston/winston.transports.ts
@@ -20,19 +20,10 @@ function createDailyRotateFile(level: string): DailyRotateFile {
   });
 }
 
-export const debugTransports: transports.ConsoleTransportInstance =
-  new transports.Console({
-    level: 'debug',
-  });
-
-export const infoTransports: transports.FileTransportInstance =
-  new transports.File(createDailyRotateFile('info'));
-
-export const warnTransports: transports.FileTransportInstance =
-  new transports.File(createDailyRotateFile('warn'));
-
-export const errorTransports: transports.FileTransportInstance =
-  new transports.File(createDailyRotateFile('error'));
-
-export const fatalTransports: transports.FileTransportInstance =
-  new transports.File(createDailyRotateFile('fatal'));
+export const debugTransports = new transports.Console({
+  level: 'debug',
+});
+export const infoTransports: DailyRotateFile = createDailyRotateFile('info');
+export const warnTransports: DailyRotateFile = createDailyRotateFile('warn');
+export const errorTransports: DailyRotateFile = createDailyRotateFile('error');
+export const fatalTransports: DailyRotateFile = createDailyRotateFile('fatal');
