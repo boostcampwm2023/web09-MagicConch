a146a4d51d97194a7b197e7424778773437be46d
Song_Minhyung <song961003@gmail.com>
2023-12-10 16:21:27 +0900

    refactor:  useControllMedia훅 별도로 사용할 수 있도록 분리

--- frontend/src/business/hooks/useWebRTC/useControllMedia.ts
+++ frontend/src/business/hooks/useWebRTC/useControllMedia.ts
@@ -3,51 +3,44 @@ import { useEffect } from 'react';
 import { useMediaInfo } from '@stores/zustandStores/useMediaInfo';
 
 import WebRTC from './WebRTC';
+import { useMedia } from './useMedia';
 
 interface useContorollMediaParams {
-  peerConnectionRef: React.MutableRefObject<RTCPeerConnection | undefined>;
   localVideoRef: React.RefObject<HTMLVideoElement | undefined>;
-  mediaInfoChannel: React.MutableRefObject<RTCDataChannel | undefined>;
-  getMedia: ({ cameraID, audioID }: { cameraID?: string; audioID?: string }) => Promise<void>;
 }
 
-export function useControllMedia({
-  localVideoRef,
-  peerConnectionRef,
-  mediaInfoChannel,
-  getMedia,
-}: useContorollMediaParams) {
-  const { selectedAudioID, selectedCameraID, setSelectedAudioID, setSelectedCameraID, toggleMyMic, toggleMyVideo } =
-    useMediaInfo(state => ({
-      toggleMyVideo: state.toggleMyVideo,
-      toggleMyMic: state.toggleMyMic,
-      setSelectedAudioID: state.setSelectedAudioID,
-      setSelectedCameraID: state.setSelectedCameraID,
-      selectedAudioID: state.selectedAudioID,
-      selectedCameraID: state.selectedCameraID,
-    }));
+const toggleTrack = (track: MediaStreamTrack) => {
+  track.enabled = !track.enabled;
+};
 
-  const webRTC = WebRTC.getInstace();
+export function useControllMedia({ localVideoRef }: useContorollMediaParams) {
+  const {
+    selectedCameraID,
+    toggleMyMic: toggleMyMicState,
+    toggleMyVideo: toggleMyVideoState,
+  } = useMediaInfo(state => ({
+    toggleMyVideo: state.toggleMyVideo,
+    toggleMyMic: state.toggleMyMic,
+    setSelectedAudioID: state.setSelectedAudioID,
+    setSelectedCameraID: state.setSelectedCameraID,
+    selectedAudioID: state.selectedAudioID,
+    selectedCameraID: state.selectedCameraID,
+  }));
 
-  const addTracks = () => {
-    if (webRTC.localStream === undefined) {
+  const { getLocalStream } = useMedia();
+
+  const {
+    dataChannels,
+    replacePeerconnectionAudioTrack2NowLocalStream,
+    replacePeerconnectionVideoTrack2NowLocalStream,
+    setLocalStream,
+  } = WebRTC.getInstace();
+
+  const setLocalVideoSrcObj = (stream: MediaStream) => {
+    if (!localVideoRef.current) {
       return;
     }
-    webRTC.localStream.getTracks().forEach(track => {
-      peerConnectionRef.current?.addTrack(track, webRTC.localStream!);
-    });
-  };
-
-  const changeVideoTrack = () => {
-    const nowTrack = webRTC.localStream?.getVideoTracks()[0];
-    const sender = peerConnectionRef.current?.getSenders().find(sender => sender.track?.kind === 'video');
-    sender?.replaceTrack(nowTrack!);
-  };
-
-  const changeAudioTrack = () => {
-    const nowTrack = webRTC.localStream?.getAudioTracks()[0];
-    const sender = peerConnectionRef.current?.getSenders().find(sender => sender.track?.kind === 'audio');
-    sender?.replaceTrack(nowTrack!);
+    localVideoRef.current.srcObject = stream;
   };
 
   const toggleVideo = () => {
@@ -56,11 +49,16 @@ export function useControllMedia({
     }
 
     const videoTrack = localVideoRef.current.srcObject as MediaStream;
-    videoTrack.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
-    toggleMyVideo();
-    // 여기서 dataChannel로 비디오 on/off를 보내줘야 함
-    if (!mediaInfoChannel.current || mediaInfoChannel.current.readyState !== 'open') return;
-    mediaInfoChannel.current.send(JSON.stringify([{ type: 'video', onOrOff: videoTrack.getVideoTracks()[0].enabled }]));
+    videoTrack.getVideoTracks().forEach(toggleTrack);
+    toggleMyVideoState();
+
+    const mediaInfoChannel = dataChannels.get('mediaInfoChannel');
+    if (!mediaInfoChannel || mediaInfoChannel.readyState !== 'open') {
+      return;
+    }
+
+    const videoTrackenabled = videoTrack.getVideoTracks()[0].enabled;
+    mediaInfoChannel.send(JSON.stringify([{ type: 'video', onOrOff: videoTrackenabled }]));
   };
 
   const toggleAudio = () => {
@@ -69,44 +67,47 @@ export function useControllMedia({
     }
 
     const audioTrack = localVideoRef.current.srcObject as MediaStream;
-    audioTrack.getAudioTracks().forEach(track => (track.enabled = !track.enabled));
-    toggleMyMic();
-    // 여기서 dataChannel로 오디오 on/off를 보내줘야 함
-    if (!mediaInfoChannel.current || mediaInfoChannel.current.readyState !== 'open') return;
-    mediaInfoChannel.current.send(JSON.stringify([{ type: 'audio', onOrOff: audioTrack.getAudioTracks()[0].enabled }]));
+    audioTrack.getAudioTracks().forEach(toggleTrack);
+    toggleMyMicState();
+
+    const mediaInfoChannel = dataChannels.get('mediaInfoChannel');
+    if (!mediaInfoChannel || mediaInfoChannel.readyState !== 'open') {
+      return;
+    }
+
+    const audioTrackenabled = audioTrack.getAudioTracks()[0].enabled;
+    mediaInfoChannel.send(JSON.stringify([{ type: 'audio', onOrOff: audioTrackenabled }]));
   };
 
-  const changeMyVideoTrack = async (id?: string) => {
-    const cameraID = id || selectedCameraID;
+  const changeMyVideoTrack = async () => {
+    const stream = await getLocalStream({ cameraID: selectedCameraID });
 
-    setSelectedCameraID(cameraID);
-    await getMedia({ cameraID });
-    changeVideoTrack();
+    setLocalVideoSrcObj(stream);
+    setLocalStream(stream);
+    replacePeerconnectionVideoTrack2NowLocalStream();
   };
 
   const changeMyAudioTrack = async (id?: string) => {
-    const audioID = id || selectedAudioID;
+    const stream = await getLocalStream({ audioID: id });
 
-    setSelectedAudioID(audioID);
-    await getMedia({ audioID: audioID });
-    changeAudioTrack();
+    setLocalVideoSrcObj(stream);
+    setLocalStream(stream);
+    replacePeerconnectionAudioTrack2NowLocalStream();
   };
 
+  // TODO mediaInfoChannel 변경되었을 떄 로직을 WebRTC 클래스로 옮기기.
   useEffect(() => {
-    if (!mediaInfoChannel.current) return;
+    const mediaInfoChannel = dataChannels.get('mediaInfoChannel');
+    if (!mediaInfoChannel) return;
 
-    mediaInfoChannel.current.addEventListener('open', () => {
+    mediaInfoChannel.addEventListener('open', () => {
       const audioTrack = localVideoRef.current?.srcObject as MediaStream;
       const videoTrack = localVideoRef.current?.srcObject as MediaStream;
 
-      mediaInfoChannel.current?.send(
-        JSON.stringify([{ type: 'audio', onOrOff: audioTrack.getAudioTracks()[0].enabled }]),
-      );
-      mediaInfoChannel.current?.send(
-        JSON.stringify([{ type: 'video', onOrOff: videoTrack.getVideoTracks()[0].enabled }]),
-      );
+      mediaInfoChannel?.send(JSON.stringify([{ type: 'audio', onOrOff: audioTrack.getAudioTracks()[0].enabled }]));
+      mediaInfoChannel?.send(JSON.stringify([{ type: 'video', onOrOff: videoTrack.getVideoTracks()[0].enabled }]));
     });
-  }, [mediaInfoChannel.current]);
+  }, []);
 
-  return { addTracks, changeMyVideoTrack, changeMyAudioTrack, toggleVideo, toggleAudio };
+  return { changeMyVideoTrack, changeMyAudioTrack, toggleVideo, toggleAudio };
 }
