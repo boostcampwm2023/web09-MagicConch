1159cf42048d8d78e91158913f29502f8ee51ba3
kimyu0218 <serena35@ewhain.net>
2024-01-09 21:55:22 +0900

    remove: 사용하지 않는 app 컨트롤러 및 서비스 삭제

--- backend/was/src/app.module.ts
+++ backend/was/src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { ChatModule } from './chat/chat.module';
 import { DatabaseModule } from './common/config/database/database.module';
 import { ErrorsInterceptor } from './common/interceptors/errors.interceptor';
 import { EventsModule } from './events/events.module';
 import { LoggerModule } from './logger/logger.module';
-import { Member } from './members/entities/member.entity';
 import { MembersModule } from './members/members.module';
-import { MembersService } from './members/members.service';
 import { TarotModule } from './tarot/tarot.module';
 
 @Module({
@@ -20,17 +16,14 @@ import { TarotModule } from './tarot/tarot.module';
     DatabaseModule,
     ChatModule,
     TarotModule,
-    TypeOrmModule.forFeature([Member]),
     EventsModule,
     LoggerModule,
   ],
-  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: ErrorsInterceptor,
     },
-    MembersService,
   ],
 })
 export class AppModule {}
