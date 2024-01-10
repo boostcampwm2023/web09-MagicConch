7462f86ac9eb6fa217506bb56f2e5442ba4ecc9b
kimyu0218 <serena35@ewhain.net>
2024-01-10 18:13:36 +0900

    feat: OAuth 관련 url 추가
    추후 사용하지 않는 url 삭제 예정

--- backend/was/src/common/constants/apis.ts
+++ backend/was/src/common/constants/apis.ts
@@ -13,6 +13,8 @@ export const OAUTH_URL = {
    */
   KAKAO_USER: 'https://kapi.kakao.com/v2/user/me',
   KAKAO_TOKEN: 'https://kauth.kakao.com/oauth/token',
+  KAKAO_ID_TOKEN: 'https://kauth.kakao.com/oauth/tokeninfo',
+  KAKAO_OIDC_USERINFO: 'https://kapi.kakao.com/v1/oidc/userinfo',
   KAKAO_LOGOUT: 'https://kapi.kakao.com/v1/user/logout',
   KAKAO_LOGOUT_ALL: 'https://kauth.kakao.com/oauth/logout',
 };
