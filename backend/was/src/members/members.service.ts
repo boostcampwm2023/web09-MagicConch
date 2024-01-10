9aacf6694abcffdb985d2e29a69806e516d23b27
kimyu0218 <serena35@ewhain.net>
2024-01-10 18:14:39 +0900

    feat: MemberService 메서드 추가
    - findByEmail : 이메일로 사용자 조회
    - updateRefreshToken : 리프레시 토큰 갱신

--- backend/was/src/members/members.service.ts
+++ backend/was/src/members/members.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateMemberDto } from './dto/create-member.dto';
 import { Member } from './entities/member.entity';
 
 @Injectable()
@@ -10,8 +11,27 @@ export class MembersService {
     private readonly membersRepository: Repository<Member>,
   ) {}
 
-  async create(): Promise<Member> {
-    const member: Member = new Member();
-    return await this.membersRepository.save(member);
+  async create(createMemberDto: CreateMemberDto): Promise<Member> {
+    try {
+      const member: Member = Member.fromDto(createMemberDto);
+      return await this.membersRepository.save(member);
+    } catch (err: unknown) {
+      throw err;
+    }
+  }
+
+  async findByEmail(email: string): Promise<Member | null> {
+    return await this.membersRepository.findOneBy({
+      email: email,
+    });
+  }
+
+  async updateRefreshToken(id: string, refreshToken: string): Promise<boolean> {
+    try {
+      await this.membersRepository.update({ id }, { refreshToken });
+      return true;
+    } catch (err: unknown) {
+      throw err;
+    }
   }
 }
