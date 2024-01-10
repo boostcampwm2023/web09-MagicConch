29c0c779c04f9f24807c80dafadf5c9567983f46
kimyu0218 <serena35@ewhain.net>
2024-01-11 05:03:52 +0900

    feat: 카카오 OAuth 관련 로직 작성
    - loginKakao : /oauth/login/kakao 요청이 들어왔을 때 실행되는 로직으로,
      인가코드를 바탕으로 토큰을 조회하고, 사용자 프로필을 조회함
      - 이미 가입된 회원이라면 login을 호출하여 사용자 정보 갱신
      - 신규 회원이라면 signup을 호출하여 사용자 정보 추가
    - logout : /oauth/logout이 들어왔을 때 실행되는 로직으로 토큰의 값을
      기준으로 서비스별 로그아웃 수행
      - 현재 verifyToken에 try~catch문 없음 (추후 가드에서 verifyToken을 수행하도록 변경할 예정이므로 수정하지 않음)
    - REST API 호출 시에 발생할 수 있는 에러 처리 추가

--- backend/was/src/auth/auth.service.ts
+++ backend/was/src/auth/auth.service.ts
@@ -1,15 +1,25 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CONTENT_TYPE, METHODS, OAUTH_URL } from 'src/common/constants/apis';
 import { ERR_MSG } from 'src/common/constants/errors';
+import { PROVIDER_ID } from 'src/common/constants/etc';
 import { CreateMemberDto } from 'src/members/dto/create-member.dto';
+import { UpdateMemberDto } from 'src/members/dto/update-member.dto';
 import { Member } from 'src/members/entities/member.entity';
 import { MembersService } from 'src/members/members.service';
-import { AccessTokenDto } from './dto/access-token.dto';
-import { KakaoAccountDto } from './dto/kakao/kakao-account.dto';
-import { KakaoTokenReqDto } from './dto/kakao/kakao-token-req.dto';
+import { JwtPayloadDto } from './dto/jwt-payload.dto';
+import { KakaoAccessTokenInfoDto } from './dto/kakao/kakao-access-token-info.dto';
 import { KakaoTokenDto } from './dto/kakao/kakao-token.dto';
+import { RefreshKakaoTokenDto } from './dto/kakao/refresh-kakao-token.dto';
+import { RequestKakaoTokenDto } from './dto/kakao/request-kakao-token.dto';
+import { OAuthTokenDto } from './dto/oauth-token.dto';
+import { ProfileDto } from './dto/profile.dto';
 
 @Injectable()
 export class AuthService {
@@ -20,6 +30,7 @@ export class AuthService {
   constructor(
     @InjectRepository(Member)
     private readonly membersService: MembersService,
+    private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {
     this.kakaoClientId = this.configService.get('KAKAO_CLIENT_ID') ?? '';
@@ -28,66 +39,94 @@ export class AuthService {
       this.configService.get('KAKAO_CLIENT_SECRET') ?? '';
   }
 
-  async loginKakao(code: string): Promise<AccessTokenDto> {
-    const token: KakaoTokenDto = await this.getKakaoToken(code);
-    const account: KakaoAccountDto = await this.getOIDCuserInfo(
-      token.access_token,
+  async loginKakao(code: string): Promise<string> {
+    const token: KakaoTokenDto = await this.requestKakaoToken(code);
+    const profile: ProfileDto = await this.getKakaoOIDCuserInfo(
+      token.id_token ?? '',
     );
     const member: Member | null = await this.membersService.findByEmail(
-      account.email,
+      profile.email,
+      PROVIDER_ID.KAKAO,
     );
     if (member) {
-      this.renewRefreshToken(member.id, token);
+      return this.login(
+        member.id,
+        PROVIDER_ID.KAKAO,
+        profile,
+        OAuthTokenDto.fromKakao(token),
+      );
     }
-    return this.signupKakao(account, token);
+    return this.signup(
+      PROVIDER_ID.KAKAO,
+      profile,
+      OAuthTokenDto.fromKakao(token),
+    );
   }
 
-  /**
-   * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#logout
-   */
-  async logoutKakao(accessToken: string): Promise<boolean> {
-    try {
-      await fetch(OAUTH_URL.KAKAO_LOGOUT, {
-        method: METHODS.POST,
-        headers: {
-          Authorization: `Authorization: Bearer ${accessToken}`,
-          'Content-type': CONTENT_TYPE.KAKAO,
-        },
-      });
-      return true;
-    } catch (err: unknown) {
-      throw new UnauthorizedException(ERR_MSG.OAUTH_KAKAO_LOGOUT_FAILED);
+  async logout(token: string) {
+    const payload: JwtPayloadDto = this.verfiyToken(token);
+    const member: Member | null = await this.membersService.findByEmail(
+      payload.email,
+      payload.providerId,
+    );
+    switch (payload.providerId) {
+      case PROVIDER_ID.KAKAO:
+        return this.logoutKakao(
+          payload.accessToken,
+          member ? member.refreshToken : '',
+        );
+      case PROVIDER_ID.NAVER:
+        break;
+      case PROVIDER_ID.GOOGLE:
+        break;
     }
   }
 
-  private async signupKakao(
-    account: KakaoAccountDto,
-    token: KakaoTokenDto,
-  ): Promise<AccessTokenDto> {
-    const createMemberDto: CreateMemberDto = CreateMemberDto.fromKakao(
+  private async signup(
+    providerId: number,
+    profile: ProfileDto,
+    token: OAuthTokenDto,
+  ): Promise<string> {
+    const createMemberDto: CreateMemberDto = CreateMemberDto.fromProfile(
+      providerId,
       token.refresh_token,
-      account,
+      profile,
     );
     await this.membersService.create(createMemberDto);
-    return { token: token.access_token, expiresIn: token.expires_in };
+    return this.makeToken(profile.email, providerId, token.access_token);
   }
 
-  private async renewRefreshToken(
+  private async login(
     id: string,
-    token: KakaoTokenDto,
-  ): Promise<AccessTokenDto> {
-    /**
-     * TODO : 무조건 갱신해도 되는 건가?
-     */
-    await this.membersService.updateRefreshToken(id, token.refresh_token);
-    return { token: token.access_token, expiresIn: token.expires_in };
+    providerId: number,
+    profile: ProfileDto,
+    token: OAuthTokenDto,
+  ): Promise<string> {
+    await this.membersService.update(
+      id,
+      UpdateMemberDto.fromProfile(providerId, token.refresh_token, profile),
+    );
+    return this.makeToken(profile.email, providerId, token.access_token);
+  }
+
+  private async logoutKakao(
+    accessToken: string,
+    refreshToken: string,
+  ): Promise<boolean> {
+    const tokenInfo: KakaoAccessTokenInfoDto | null =
+      await this.getKakaoAccessTokenInfo(accessToken);
+    if (tokenInfo) {
+      return this.requestKakaoLogout(accessToken);
+    }
+    const newToken: KakaoTokenDto = await this.refreshKakaoToken(refreshToken);
+    return this.requestKakaoLogout(newToken.access_token);
   }
 
   /**
    * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
    */
-  private async getKakaoToken(code: string): Promise<KakaoTokenDto> {
-    const reqBody: KakaoTokenReqDto = KakaoTokenReqDto.fromInfo(
+  private async requestKakaoToken(code: string): Promise<KakaoTokenDto> {
+    const reqBody: RequestKakaoTokenDto = RequestKakaoTokenDto.fromInfo(
       this.kakaoClientId,
       this.kakaoRedirectUri,
       code,
@@ -106,19 +145,102 @@ export class AuthService {
   }
 
   /**
+   * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#refresh-token
+   */
+  private async refreshKakaoToken(
+    refreshToken: string,
+  ): Promise<KakaoTokenDto> {
+    const reqBody: RefreshKakaoTokenDto = RefreshKakaoTokenDto.fromInfo(
+      this.kakaoClientId,
+      refreshToken,
+      this.kakaoClientSecret,
+    );
+    try {
+      const res: any = await fetch(OAUTH_URL.KAKAO_TOKEN, {
+        method: METHODS.POST,
+        headers: { 'Content-type': CONTENT_TYPE.KAKAO },
+        body: JSON.stringify(reqBody),
+      });
+      return (await res.json()) as KakaoTokenDto;
+    } catch (err: unknown) {
+      throw new UnauthorizedException(ERR_MSG.OAUTH_KAKAO_TOKEN_FAILED);
+    }
+  }
+
+  /**
    * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#oidc-user-info
    */
-  private async getOIDCuserInfo(accessToken: string): Promise<KakaoAccountDto> {
-    try {
-      const res: any = await fetch(OAUTH_URL.KAKAO_OIDC_USERINFO, {
-        headers: { Authorization: `Authorization: Bearer ${accessToken}` },
-      });
-      const resBody: any = await res.json();
-      return KakaoAccountDto.fromOIDCuserInfo(resBody);
-    } catch (err: unknown) {
-      throw new UnauthorizedException(
-        ERR_MSG.OAUTH_KAKAO_OIDC_USER_INFO_FAILED,
-      );
+  private async getKakaoOIDCuserInfo(idToken: string): Promise<ProfileDto> {
+    const res: any = await fetch(OAUTH_URL.KAKAO_OIDC_USERINFO, {
+      headers: { 'Content-type': CONTENT_TYPE.KAKAO },
+      body: JSON.stringify({ id_token: idToken }),
+    });
+    const resBody: any = await res.json();
+    return ProfileDto.fromKakao(resBody);
+  }
+
+  /**
+   * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#get-token-info
+   */
+  private async getKakaoAccessTokenInfo(
+    accessToken: string,
+  ): Promise<KakaoAccessTokenInfoDto | null> {
+    const res: any = await fetch(OAUTH_URL.KAKAO_ACCESS_TOKEN, {
+      headers: { Authorization: `Authorization: Bearer ${accessToken}` },
+    });
+    const detail: any = { status: res.code, body: await res.json() };
+    if (detail.status === 200) {
+      return detail.body as KakaoAccessTokenInfoDto;
     }
+    return this.handleKakaoAccessTokenInfoError(
+      detail.status,
+      detail.body.code,
+    );
+  }
+
+  /**
+   * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#logout
+   */
+  private async requestKakaoLogout(accessToken: string): Promise<boolean> {
+    await fetch(OAUTH_URL.KAKAO_LOGOUT, {
+      method: METHODS.POST,
+      headers: {
+        Authorization: `Authorization: Bearer ${accessToken}`,
+        'Content-type': CONTENT_TYPE.KAKAO,
+      },
+    });
+    return true;
+  }
+
+  private handleKakaoAccessTokenInfoError(status: number, code: number): null {
+    if (status === 401) {
+      return null;
+    }
+    throw new InternalServerErrorException(
+      code === -1
+        ? ERR_MSG.OAUTH_KAKAO_ACCESS_TOKEN_INFO_KAKAO_ERROR
+        : ERR_MSG.OAUTH_KAKAO_ACCESS_TOKEN_INFO_BAD_REQUEST,
+    );
+  }
+
+  private makeToken(
+    email: string,
+    providerId: number,
+    accessToken: string,
+  ): string {
+    const payload: JwtPayloadDto = JwtPayloadDto.fromInfo(
+      email,
+      providerId,
+      accessToken,
+    );
+    return this.signPayload(payload);
+  }
+
+  private signPayload(payload: JwtPayloadDto): string {
+    return this.jwtService.sign(payload);
+  }
+
+  private verfiyToken(token: string): JwtPayloadDto {
+    return this.jwtService.verify(token);
   }
 }
