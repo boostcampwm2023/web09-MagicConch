479dbf721107317a2a67ba21078e8b389e235300
kimyu0218 <serena35@ewhain.net>
2024-01-11 05:11:10 +0900

    feat: AuthController 수정
    - jwt를 쿠키에 부착하도록 구현
      - httpOnly로 XSS 공격 방지
      - prod 환경에서 secure를 사용하여 암호화된 통신 이용하도록 설정
      - sameSite 설정 CSRF 공격 예방
    - logout 추가

--- backend/was/src/auth/auth.controller.ts
+++ backend/was/src/auth/auth.controller.ts
@@ -5,39 +5,45 @@ import {
   Res,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 import { ERR_MSG } from 'src/common/constants/errors';
 import { AuthService } from './auth.service';
-import { AccessTokenDto } from './dto/access-token.dto';
 
 @Controller('oauth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  private readonly isProd: boolean;
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    this.isProd = this.configService.get('ENV') === 'PROD';
+  }
 
   @Get('login/kakao')
   async kakaoLogin(@Req() req: Request, @Res() res: Response): Promise<void> {
-    /**
-     * TODO : 에러 메시지를 전송해도 괜찮은가?
-     */
     if (req.params.error) {
       throw new UnauthorizedException(ERR_MSG.OAUTH_KAKAO_AUTH_CODE_FAILED);
     }
-    const accessToken: AccessTokenDto = await this.authService.loginKakao(
-      req.params.code,
-    );
-    res.cookie('mcat', accessToken.token, {
+    const jwt: string = await this.authService.loginKakao(req.params.code);
+    res.cookie('magicconch', jwt, {
       httpOnly: true,
+      secure: this.isProd,
       sameSite: 'strict',
-      maxAge: accessToken.expiresIn,
     });
     res.sendStatus(200);
   }
 
   /**
-   * TODO : 가드 수정 후 추가
+   * TODO : 가드 수정 후 적용 필요
    */
-  @Get('logout/kakao')
-  async kakaoLogout(@Req() req: Request): Promise<void> {
-    await this.authService.logoutKakao(req.cookies.mct ?? '');
+  @Get('logout')
+  async kakaoLogout(@Req() req: Request, @Res() res: Response): Promise<void> {
+    await this.authService.logout(req.cookies.magicconch);
+    res.clearCookie('magicconch', {
+      httpOnly: true,
+      secure: this.isProd,
+      sameSite: 'strict',
+    });
   }
 }
