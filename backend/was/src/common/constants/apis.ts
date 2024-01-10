acbb2350e7348ed6550fe5f78d4569c9f3351aad
kimyu0218 <serena35@ewhain.net>
2024-01-11 05:00:55 +0900

    feat: 카카오 액세스 토큰 정보 조회하는 url 추가

--- backend/was/src/common/constants/apis.ts
+++ backend/was/src/common/constants/apis.ts
@@ -14,6 +14,7 @@ export const OAUTH_URL = {
   KAKAO_USER: 'https://kapi.kakao.com/v2/user/me',
   KAKAO_TOKEN: 'https://kauth.kakao.com/oauth/token',
   KAKAO_ID_TOKEN: 'https://kauth.kakao.com/oauth/tokeninfo',
+  KAKAO_ACCESS_TOKEN: 'https://kapi.kakao.com/v1/user/access_token_info',
   KAKAO_OIDC_USERINFO: 'https://kapi.kakao.com/v1/oidc/userinfo',
   KAKAO_LOGOUT: 'https://kapi.kakao.com/v1/user/logout',
   KAKAO_LOGOUT_ALL: 'https://kauth.kakao.com/oauth/logout',
