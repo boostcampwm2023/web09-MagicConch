028ee9c7a56a923b113ddcc126f760c8cbeae960
kimyu0218 <serena35@ewhain.net>
2024-01-11 05:03:05 +0900

    feat: JwtModule 관련 로직 추가
    AuthService에서 JwtService를 사용할 수 있도록 환경변수 세팅 및 JwtModule
    추가

--- backend/was/src/auth/auth.module.ts
+++ backend/was/src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/members/entities/member.entity';
 import { MembersService } from 'src/members/members.service';
@@ -6,7 +8,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET_KEY'),
+          signOptions: { expiresIn: 'JWT_EXPIRES_IN' },
+        };
+      },
+    }),
+    TypeOrmModule.forFeature([Member]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, MembersService],
 })
