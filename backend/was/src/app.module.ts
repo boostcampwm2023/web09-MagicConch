dba5782f1fee63c2d3154d0c3842571a78379ef6
kimyu0218 <serena35@ewhain.net>
2024-01-11 23:29:51 +0900

    feat: auth 모듈 추가

--- backend/was/src/app.module.ts
+++ backend/was/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { DatabaseModule } from './common/config/database/database.module';
 import { ErrorsInterceptor } from './common/interceptors/errors.interceptor';
@@ -8,7 +9,6 @@ import { EventsModule } from './events/events.module';
 import { LoggerModule } from './logger/logger.module';
 import { MembersModule } from './members/members.module';
 import { TarotModule } from './tarot/tarot.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
