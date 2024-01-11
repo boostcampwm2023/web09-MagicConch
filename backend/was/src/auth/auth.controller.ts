39b729237d6d4378d06704c99280dd8c6a38a230
kimyu0218 <serena35@ewhain.net>
2024-01-11 18:16:42 +0900

    feat: AuthController 쿠키 부착
    - 로그인에 성공한 경우, magicconch라는 쿠키 부착하도록 설정
    - 로그아웃 시 가장 먼저 AuthGuard를 통해 인증 여부 확인
    - req에 붙은 user 값을 바탕으로 어느 OAuth를 로그아웃 해야하는지 판단
    - OAuth 로그아웃에 성공하면 서비스 로그아웃 처리

--- backend/was/src/auth/auth.controller.ts
+++ backend/was/src/auth/auth.controller.ts
@@ -4,18 +4,22 @@ import {
   Req,
   Res,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 import { ERR_MSG } from 'src/common/constants/errors';
-import { AuthService } from './auth.service';
+import { PROVIDER_ID } from 'src/common/constants/etc';
+import { JwtPayloadDto } from './dto/jwt-payload.dto';
+import { AuthGuard } from './guard/auth.guard';
+import { KakaoAuthService } from './service/kakao.auth.service';
 
 @Controller('oauth')
 export class AuthController {
   private readonly isProd: boolean;
   constructor(
-    private readonly authService: AuthService,
     private readonly configService: ConfigService,
+    private readonly kakaoAuthService: KakaoAuthService,
   ) {
     this.isProd = this.configService.get('ENV') === 'PROD';
   }
@@ -25,7 +29,7 @@ export class AuthController {
     if (req.params.error) {
       throw new UnauthorizedException(ERR_MSG.OAUTH_KAKAO_AUTH_CODE_FAILED);
     }
-    const jwt: string = await this.authService.loginKakao(req.params.code);
+    const jwt: string = await this.kakaoAuthService.loginOAuth(req.params.code);
     res.cookie('magicconch', jwt, {
       httpOnly: true,
       secure: this.isProd,
@@ -34,16 +38,24 @@ export class AuthController {
     res.sendStatus(200);
   }
 
-  /**
-   * TODO : 가드 수정 후 적용 필요
-   */
+  @UseGuards(AuthGuard)
   @Get('logout')
-  async kakaoLogout(@Req() req: Request, @Res() res: Response): Promise<void> {
-    await this.authService.logout(req.cookies.magicconch);
+  async kakaoLogout(@Req() req: any, @Res() res: Response): Promise<void> {
+    const user: JwtPayloadDto = req.user;
+    switch (user.providerId) {
+      case PROVIDER_ID.KAKAO:
+        await this.kakaoAuthService.logoutOAuth(user);
+        break;
+      case PROVIDER_ID.NAVER:
+        break;
+      case PROVIDER_ID.GOOGLE:
+        break;
+    }
     res.clearCookie('magicconch', {
       httpOnly: true,
       secure: this.isProd,
       sameSite: 'strict',
     });
+    res.sendStatus(200);
   }
 }
