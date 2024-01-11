0a5aff1d305a81622727f6dbf8dd903a7b98c9db
kimyu0218 <serena35@ewhain.net>
2024-01-11 18:13:01 +0900

    feat: PROVIDER_NAME 추가
    OAuth를 지원하는 플랫폼 이름 상수화

--- backend/was/src/common/constants/etc.ts
+++ backend/was/src/common/constants/etc.ts
@@ -1,6 +1,16 @@
-export const BUCKET_URL = 'https://kr.object.ncloudstorage.com/magicconch';
-export const PROVIDER_ID = {
+export const BUCKET_URL: string =
+  'https://kr.object.ncloudstorage.com/magicconch';
+
+type Provider = 'KAKAO' | 'NAVER' | 'GOOGLE';
+
+export const PROVIDER_ID: Record<Provider, number> = {
   KAKAO: 0,
   NAVER: 1,
   GOOGLE: 2,
+};
+
+export const PROVIDER_NAME: Record<Provider, Provider> = {
+  KAKAO: 'KAKAO',
+  NAVER: 'NAVER',
+  GOOGLE: 'GOOGLE',
 };
