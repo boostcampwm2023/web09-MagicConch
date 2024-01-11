46bd048a6114d9ab4f060d365ea1b2c2d03e5ec7
kimyu0218 <serena35@ewhain.net>
2024-01-11 23:17:57 +0900

    feat: CacheConfigModule 작성
    redis를 이용하는 CacheConfigModule

---
+++ backend/was/src/common/config/cache/cache.module.ts
@@ -0,0 +1,27 @@
+import { CacheModule } from '@nestjs/cache-manager';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
+
+@Module({})
+export class CacheConfigModule {
+  static register(): DynamicModule {
+    return {
+      module: CacheConfigModule,
+      imports: [
+        CacheModule.registerAsync({
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService): RedisClientOptions => {
+            return {
+              store: redisStore,
+              host: configService.get('CACHE_HOST'),
+              port: configService.get('CACHE_PORT'),
+            } as RedisClientOptions;
+          },
+        }),
+      ],
+      exports: [CacheModule],
+    };
+  }
+}
