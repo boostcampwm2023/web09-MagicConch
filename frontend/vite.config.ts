dbe3377884540d94ac3c0c048d2e2e35408351a6
Song_Minhyung <song961003@gmail.com>
2024-01-15 18:32:39 +0900

    chore: vitest 설치, 설정 완료
    - 최신 vitest를 사용하기위해 vite 버전 5로 올림

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
-import react from '@vitejs/plugin-react-swc';
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
 });
