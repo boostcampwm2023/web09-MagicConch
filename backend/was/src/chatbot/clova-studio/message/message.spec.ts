c42b0ce5c0e76e65ca052fe23d7c51534f6b6580
HeoJiye <heoze1031@gmail.com>
2024-01-15 10:07:14 +0900

    test: chatLog2clovaStudioMessages 함수 테스트 코드 작성

--- backend/was/src/chatbot/clova-studio/message/message.spec.ts
+++ backend/was/src/chatbot/clova-studio/message/message.spec.ts
@@ -1,16 +1,30 @@
-import {
-  chatLog2clovaStudioMessages,
-  clovaStudioMessages2chatLog,
-} from './converter';
+import { TALK_SYSTEM_MESSAGE } from 'src/common/constants/clova-studio';
+import { ChatLog } from 'src/common/types/chatbot';
+import { ClovaStudioMessage } from 'src/common/types/clova-studio';
+import { chatLog2clovaStudioMessages } from './converter';
 import { createTarotCardMessage, createUserMessage } from './creator';
 
-describe('[chatbot/clova-studio/stream]', () => {
+describe('[chatbot/clova-studio/message]', () => {
   describe('function chatLog2clovaStudioMessages()', () => {
-    it('test', () => expect(false).toBe(true));
-  });
+    it('test (1)', () => {
+      const input: ChatLog = [
+        { isHost: true, message: 'host message' },
+        { isHost: false, message: 'user message' },
+      ];
+      const output: ClovaStudioMessage[] = [
+        { role: 'assistant', content: 'host message' },
+        { role: 'user', content: 'user message' },
+      ];
 
-  describe('function clovaStudioMessages2chatLog()', () => {
-    it('test', () => expect(false).toBe(true));
+      expect(chatLog2clovaStudioMessages(input)).toEqual(output);
+    });
+
+    it('test (2)', () => {
+      const input: ChatLog = [];
+      const output: ClovaStudioMessage[] = [];
+
+      expect(chatLog2clovaStudioMessages(input)).toEqual(output);
+    });
   });
 
   describe('function createTarotCardMessage()', () => {
@@ -18,6 +32,14 @@ describe('[chatbot/clova-studio/stream]', () => {
   });
 
   describe('function createUserMessage()', () => {
+    it('test', () => expect(false).toBe(true));
+  });
+
+  describe('function buildTalkMessages()', () => {
+    it('test', () => expect(false).toBe(true));
+  });
+
+  describe('function buildTarotReadingMessages()', () => {
     it('test', () => expect(false).toBe(true));
   });
 });
