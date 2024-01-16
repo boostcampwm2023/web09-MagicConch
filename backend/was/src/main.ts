cce45caf2301bfcd9f4a73120c29892920d9cbba
kimyu0218 <serena35@ewhain.net>
2024-01-16 14:28:36 +0900

    fix: graceful shutdown 로직 수정
    - enableShutdown()으로 셧다운 훅 사용
    - http server 닫을 때 await 붙여서 순차적으로 이루어지도록 수정

--- backend/was/src/main.ts
+++ backend/was/src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
@@ -7,23 +7,23 @@ import { setupSwagger } from './common/config/swagger.setting';
 import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.enableShutdownHooks();
 
-  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
 
   const logger: LoggerService = app.get(LoggerService);
   app.useLogger(logger);
 
   const dsn: string = process.env.SENTRY_DSN || '';
   setupSentry(app, dsn);
-
   setupSwagger(app);
 
   const port: number = parseInt(process.env.PORT || '3000');
@@ -32,7 +32,7 @@ async function bootstrap() {
   process.on('SIGTERM', async () => {
     logger.log('🖐️ Received SIGTERM signal. Start Graceful Shutdown...');
 
-    server.close();
+    await server.close();
     await app.close();
 
     logger.log('🖐️ Nest Application closed gracefully...');
