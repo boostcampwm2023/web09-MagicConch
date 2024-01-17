61511c7ebdeefa4939e80b1634c1f55ac2d36792
kimyu0218 <serena35@ewhain.net>
2024-01-18 01:58:49 +0900

    feat: passport를 위한 설정 추가

--- backend/was/src/auth/auth.module.ts
+++ backend/was/src/auth/auth.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/members/entities';
 import { MembersModule } from 'src/members/members.module';
 import { MembersService } from 'src/members/members.service';
 import { AuthController } from './auth.controller';
-import { AuthGuard } from './guard/auth.guard';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
 import { AuthService } from './service/auth.service';
 import { KakaoAuthService } from './service/kakao.auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member]), MembersModule],
+  imports: [PassportModule, TypeOrmModule.forFeature([Member]), MembersModule],
   controllers: [AuthController],
-  providers: [AuthService, AuthGuard, KakaoAuthService, MembersService],
+  providers: [
+    AuthService,
+    KakaoAuthService,
+    MembersService,
+    JwtStrategy,
+    JwtAuthGuard,
+  ],
+  exports: [PassportModule, JwtAuthGuard],
 })
 export class AuthModule {}
