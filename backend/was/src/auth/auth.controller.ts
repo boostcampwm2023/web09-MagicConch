474cb4ee6e1815fa8576ea4d3211182bb402fb3e
kimyu0218 <serena35@ewhain.net>
2024-01-19 00:11:18 +0900

    feat: Authorization Bearer <token> 붙여주도록 설정
    - socket.io 공식문서에 따르면 socket에서 handshake 객체에 접근할 수 있음
    - handshake 안에 headers, auth 등의 요청 객체의 일부 정보들이 포함되어
      있음
    - socket.io에서 특정 쿠키에 접근하는 방법이 없는 것 같아 Authorization
      헤더에 붙여주도록 구현함
    - 추후 socket.io 동작 상황을 보고 수정할 예정

--- backend/was/src/auth/auth.controller.ts
+++ backend/was/src/auth/auth.controller.ts
@@ -44,6 +44,12 @@ export class AuthController {
     const jwt: string = await this.kakaoAuthService.loginOAuth(
       req.query.code as string,
     );
+    /**
+     * TODO : socket.io에서 헤더 접근 확인 후에 수정 필요
+     * - socket.io의 handshake에서 접근할 수 있는 값이 정해져 있음
+     * - cookie는 접근을 못하는 듯 하여 authorization 헤더에 jwt 붙여주도록 구현
+     */
+    res.setHeader('Authorization', `Bearer ${jwt}`);
     res.cookie('magicconch', jwt, this.cookieOptions);
     res.sendStatus(200);
   }
@@ -62,6 +68,7 @@ export class AuthController {
       case PROVIDER_ID.GOOGLE:
         break;
     }
+    res.removeHeader('Authorization');
     res.clearCookie('magicconch');
     res.sendStatus(200);
   }
