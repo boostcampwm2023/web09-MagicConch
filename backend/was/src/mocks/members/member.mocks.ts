2657feddc45c02d4d1f7c82912cd8e5070c2d5a5
kimyu0218 <serena35@ewhain.net>
2024-01-19 05:42:31 +0900

    feat: MemberService 테스트를 위해 추가적으로 필요한 mock 정의

--- backend/was/src/mocks/members/member.mocks.ts
+++ backend/was/src/mocks/members/member.mocks.ts
@@ -1,13 +1,60 @@
+import { CreateMemberDto, UpdateMemberDto } from 'src/members/dto';
 import { Member } from 'src/members/entities';
 
-function makeMemberMock(memberId: string): Member {
+function makeMemberMock(
+  memberId: string,
+  email: string,
+  nickname: string,
+  profileUrl: string,
+  providerId: number,
+): Member {
   const memberMock: Member = new Member();
   memberMock.id = memberId;
+  memberMock.email = email;
+  memberMock.nickname = nickname;
+  memberMock.profileUrl = profileUrl;
+  memberMock.providerId = providerId;
   return memberMock;
 }
+
+const nickname: string = 'nickname';
+
+const diffNickname: string = 'diffNickname';
+
+const profileUrl: string = 'profileUrl';
+
+export const email: string = 'email';
+
+export const providerId: number = 0;
+
+export const diffProviderId: number = 1;
 
 export const memberId: string = 'memberId';
 
 export const diffMemberId: string = 'diffMemberId';
 
-export const memberMock: Member = makeMemberMock(memberId);
+export const memberMock: Member = makeMemberMock(
+  memberId,
+  email,
+  nickname,
+  profileUrl,
+  providerId,
+);
+
+export const createMemberDtoMock: CreateMemberDto = CreateMemberDto.fromProfile(
+  providerId,
+  {
+    email: email,
+    nickname: nickname,
+    profileUrl: profileUrl,
+  },
+);
+
+export const updateMemberDtoMock: UpdateMemberDto = UpdateMemberDto.fromProfile(
+  providerId,
+  {
+    email: email,
+    nickname: diffNickname,
+    profileUrl: profileUrl,
+  },
+);
