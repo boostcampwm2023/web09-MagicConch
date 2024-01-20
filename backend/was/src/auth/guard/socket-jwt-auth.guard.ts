c96e31b9503c8567ad43629cd3776b9a651a23ed
kimyu0218 <serena35@ewhain.net>
2024-01-20 16:59:03 +0900

    feat: 소켓용 가드 작성
    - 웹소켓에서도 가드를 사용할 수 있음
    - switchToHttp() 대신 switchToWs()로 웹소켓에 대해 처리하도록 구현
    - 현재 헤더의 authorization에 들어가 있다고 가정하고 구현
      -> 추후 클라이언트 카카오 로그인 완료된 후에 header에 잘 붙어오는지
      확인 필요
    - token이 없는 사용자는 로그인하지 않은 사용자로 간주하고 곧바로 true
      반환
    - token이 있는 사용자는 로그인 한 사용자이므로 client에 정보 저장한 후에
      true 반환

---
+++ backend/was/src/auth/guard/socket-jwt-auth.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { JwtPayloadDto } from '../dto';
+
+@Injectable()
+export class SocketJwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const client = context.switchToWs().getClient();
+    const token: string | null = client.handshake.headers.authorization;
+    if (!token) {
+      return true;
+    }
+    try {
+      const decodedToken: JwtPayloadDto = this.verifyToken(token);
+      client.user = decodedToken;
+      return true;
+    } catch (err: unknown) {
+      throw err;
+    }
+  }
+
+  private verifyToken(token: string): JwtPayloadDto {
+    return this.jwtService.verify(token);
+  }
+}
