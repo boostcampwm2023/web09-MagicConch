b1a6c17dd8a43f7130b0d095ff5ff9ed9e2db453
Song_Minhyung <song961003@gmail.com>
2024-01-22 18:53:29 +0900

    test: mediaStreamMock 이름 한눈에 알도록 변경

--- frontend/src/mocks/webRTC/mediaStreamMock.ts
+++ frontend/src/mocks/webRTC/mediaStreamMock.ts
@@ -1,37 +1,37 @@
 export const createFakeMediaStreamTrack = (kind: 'video' | 'audio', id: string): any => ({
   kind,
   id,
-  enabled: false,
+  enabled: true,
 });
 
 export const mockMediaStream = {
   getTracks: vi
     .fn()
     .mockImplementation(() => [
-      createFakeMediaStreamTrack('video', 'videoTrack1'),
-      createFakeMediaStreamTrack('video', 'videoTrack2'),
-      createFakeMediaStreamTrack('audio', 'audioTrack1'),
-      createFakeMediaStreamTrack('audio', 'audioTrack2'),
+      createFakeMediaStreamTrack('video', 'video1'),
+      createFakeMediaStreamTrack('video', 'video2'),
+      createFakeMediaStreamTrack('audio', 'audio1'),
+      createFakeMediaStreamTrack('audio', 'audio2'),
     ]),
   getVideoTracks: vi
     .fn()
     .mockImplementation(() => [
-      createFakeMediaStreamTrack('video', 'videoTrack1'),
-      createFakeMediaStreamTrack('video', 'videoTrack2'),
+      createFakeMediaStreamTrack('video', 'video1'),
+      createFakeMediaStreamTrack('video', 'video2'),
     ]),
   getAudioTracks: vi
     .fn()
     .mockImplementation(() => [
-      createFakeMediaStreamTrack('audio', 'audioTrack1'),
-      createFakeMediaStreamTrack('audio', 'audioTrack2'),
+      createFakeMediaStreamTrack('audio', 'audio1'),
+      createFakeMediaStreamTrack('audio', 'audio2'),
     ]),
 } as any as MediaStream;
 
 export const mockRTCTrackEvent = {
   streams: [
-    createFakeMediaStreamTrack('video', 'videoTrack1'),
-    createFakeMediaStreamTrack('video', 'videoTrack2'),
-    createFakeMediaStreamTrack('audio', 'audioTrack1'),
-    createFakeMediaStreamTrack('audio', 'audioTrack2'),
+    createFakeMediaStreamTrack('video', 'video1'),
+    createFakeMediaStreamTrack('video', 'video2'),
+    createFakeMediaStreamTrack('audio', 'audio1'),
+    createFakeMediaStreamTrack('audio', 'audio2'),
   ],
 };
