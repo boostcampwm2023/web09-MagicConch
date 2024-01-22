21c3425c91b720c7fcc380ba58225aeba9308599
kimyu0218 <serena35@ewhain.net>
2024-01-22 22:11:08 +0900

    feat: 배포 스크립트 에러 핸들링 보완
    - 상단에 "set -e"를 추가하여 에러가 발생하면 스크립트가 즉시 종료되도록
      설정
    - trap을 이용하여 ERR 시그널이 발생하면 에러 메시지를 출력하고
      종료하도록 설정
    - docker-compose, reload nginx 중에 에러가 발생하면 곧바로 종료

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
+ERR_MSG=''
+
+trap 'echo "Error occured: $ERR_MSG. Exiting deploy script."; exit 1' ERR
+
 if sudo docker ps --filter "name=blue" --format '{{.ID}}' | grep -E .; then
   RUN_TARGET="green"
   STOP_TARGET="blue"
@@ -15,8 +21,8 @@ fi
 echo "The $STOP_TARGET version is currently running on the server. Starting the $RUN_TARGET version."
 
 DOCKER_COMPOSE_FILE="compose.$RUN_TARGET-deploy.yml"
-sudo docker-compose -f "$DOCKER_COMPOSE_FILE" pull || exit 1
-sudo docker-compose -f "$DOCKER_COMPOSE_FILE" up -d || exit 1
+sudo docker-compose -f "$DOCKER_COMPOSE_FILE" pull || { ERR_MSG='Failed to pull docker image'; exit 1; }
+sudo docker-compose -f "$DOCKER_COMPOSE_FILE" up -d || { ERR_MSG='Failed to start docker image'; exit 1; }
 sleep 50
 
 echo "Starting health check for the new version of the application."
@@ -40,7 +46,7 @@ if [ "$HEALTH_CHECK_PASSED" = true ]; then
 
   sudo docker exec $NGINX_ID /bin/bash -c "sed -i 's/was-$STOP_TARGET:$WAS_STOP_PORT/was-$RUN_TARGET:$WAS_RUN_PORT/' $NGINX_CONFIG"
   sudo docker exec $NGINX_ID /bin/bash -c "sed -i 's/signal-$STOP_TARGET:$((WAS_STOP_PORT + 1))/signal-$RUN_TARGET:$((WAS_RUN_PORT + 1))/' $NGINX_CONFIG"
-  sudo docker exec $NGINX_ID /bin/bash -c "nginx -s reload"
+  sudo docker exec $NGINX_ID /bin/bash -c "nginx -s reload" || { ERR_MSG='Failed to reload nginx'; exit 1; }
 
   echo "Terminating the $STOP_TARGET applications."
 
