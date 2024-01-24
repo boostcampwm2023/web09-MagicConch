634314060181d0c667d61d0d46bbfbe7784f5b34
Song_Minhyung <song961003@gmail.com>
2024-01-24 18:32:29 +0900

    test: SocketManager Mock 수정
    - 함수형 -> class로

--- frontend/src/business/services/SocketManager/__mocks__/SocketManager.ts
+++ frontend/src/business/services/SocketManager/__mocks__/SocketManager.ts
@@ -1,6 +1,6 @@
-export const SocketManager = {
-  on: vi.fn(),
-  emit: vi.fn(),
-  connect: vi.fn(),
-  disconnect: vi.fn(),
-};
+export class SocketManager {
+  on = vi.fn();
+  emit = vi.fn();
+  connect = vi.fn();
+  disconnect = vi.fn();
+}
