ee4a110bcbc9690e220b4aa8fa2881b666125675
HeoJiye <heoze1031@gmail.com>
2024-01-24 17:17:11 +0900

    feat: ai에게 타로보기 버튼 클릭하면 로그인 팝업 띄우기

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 
 import Background from '@components/Background';
-import { Button, KakaoLoginButton } from '@components/Buttons';
+import { Button } from '@components/Buttons';
 
 import { useLoginPopup } from '@business/hooks/usePopup/useLoginPopup';
 
 function HomePage() {
   const navigate = useNavigate();
-  const { openLoginPopup } = useLoginPopup();
 
-  const moveAiChat = () => {
-    // navigate('/chat/ai');
-    openLoginPopup();
-  };
+  const moveAiChat = () => navigate('/chat/ai');
+
+  const { openLoginPopup } = useLoginPopup({ moveAiChat });
 
   const moveHumanChat = () => {
     navigate('/chat/human', { state: { host: true } });
@@ -20,17 +18,14 @@ function HomePage() {
 
   return (
     <Background>
-      <div>
-        <KakaoLoginButton />
-        <div className="relative top-75 flex gap-36 z-1">
-          <Button onClick={moveAiChat}>AI에게 타로보기</Button>
-          <Button
-            onClick={moveHumanChat}
-            color="dark"
-          >
-            채팅방 개설하기
-          </Button>
-        </div>
+      <div className="relative top-75 flex gap-36 z-1">
+        <Button onClick={openLoginPopup}>AI에게 타로보기</Button>
+        <Button
+          onClick={moveHumanChat}
+          color="dark"
+        >
+          채팅방 개설하기
+        </Button>
       </div>
     </Background>
   );
