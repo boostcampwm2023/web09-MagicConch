0ce8d9a1a708817ce7a8ad318aff41c988070942
HeoJiye <heoze1031@gmail.com>
2024-01-26 15:53:50 +0900

    feat: sidebar 관련 zustand store 작성

---
+++ frontend/src/stores/zustandStores/useSideBarStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+interface SideBarState {
+  sideBarState: boolean;
+  sideBarButtonState: boolean;
+}
+
+interface SideBarActions {
+  showSideBar: () => void;
+  hideSideBar: () => void;
+  activeSideBarButton: () => void;
+  deactiveSideBarButton: () => void;
+}
+
+const initialState: SideBarState = {
+  sideBarState: false,
+  sideBarButtonState: true,
+};
+
+export const useSideBarStore = create<SideBarState & SideBarActions>()(
+  devtools(set => ({
+    ...initialState,
+    showSideBar: () => set(() => ({ sideBarState: true })),
+    hideSideBar: () => set(() => ({ sideBarState: false })),
+    activeSideBarButton: () => set(() => ({ sideBarButtonState: true })),
+    deactiveSideBarButton: () => set(() => ({ sideBarButtonState: false })),
+  })),
+);
+
+export function initSideBarStore() {
+  useSideBarStore.setState(initialState);
+}
