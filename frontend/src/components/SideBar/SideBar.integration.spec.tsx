e420164a0a86e5f3137d8a0ea7c874b9762a858b
HeoJiye <heoze1031@gmail.com>
2024-01-26 18:01:24 +0900

    test: sidebar 통합 테스트 (상태가 바뀌는 경우 테스트 작성)

--- frontend/src/components/SideBar/SideBar.integration.spec.tsx
+++ frontend/src/components/SideBar/SideBar.integration.spec.tsx
@@ -2,7 +2,7 @@ import { render, renderHook } from '@testing-library/react';
 
 import { initialState, useSideBarStore } from '@stores/zustandStores/useSideBarStore';
 
-import { toBeCenterOfScreenX } from '@utils/matcher';
+import { toBeCenterOfScreenX, toBeLeftOfScreenX } from '@utils/matcher';
 import { sleep } from '@utils/time';
 
 import ContentAreaWithSideBar from './ContentAreaWithSideBar';
@@ -21,7 +21,7 @@ const IntegrationComponent = () => {
   );
 };
 
-expect.extend({ toBeCenterOfScreenX });
+expect.extend({ toBeCenterOfScreenX, toBeLeftOfScreenX });
 
 describe('SideBar 관련 컴포넌트 통합 테스트', () => {
   describe('처음 렌더링 이후', () => {
@@ -68,12 +68,112 @@ describe('SideBar 관련 컴포넌트 통합 테스트', () => {
   });
 
   describe('상태가 바뀌는 경우', () => {
-    it('애니메이션 효과가 발생한다.', () => {});
-    it('side bar hide 상태에서 side bar 버튼을 클릭하면, side bar가 화면에 보여진다.', () => {});
-    it('side bar hide 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙에서 왼쪽으로 이동된다.', () => {});
-    it('side bar show 상태에서 side bar 버튼을 클릭하면, side bar는 화면에서 사라진다.', () => {});
-    it('side bar show 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙으로 이동된다.', () => {});
-    it('side bar hide 상태에서 side bar 버튼이 비활성화되면 버튼을 클릭해도 작동하지 않는다.', () => {});
-    it('side bar show 상태에서 side bar 버튼이 비활성화되면 content area가 x축 기준 화면 중앙으로 이동하면서 side bar가 화면에서 사라지고, 버튼을 클릭해도 작동하지 않는다.', () => {});
+    it('side bar hide 상태에서 side bar 버튼을 클릭하면, side bar가 화면에 보여진다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const sideBar = await findByText('side bar');
+      const sideBarButton = await findByLabelText('button');
+
+      expect(sideBar).not.toBeVisible();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).toBeVisible();
+    });
+
+    it('side bar hide 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙에서 왼쪽으로 이동된다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const contentArea = await findByText('content area');
+      const sideBarButton = await findByLabelText('button');
+
+      expect(contentArea).toBeCenterOfScreenX();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(contentArea).toBeLeftOfScreenX();
+    });
+
+    it('side bar show 상태에서 side bar 버튼을 클릭하면, side bar는 화면에서 사라진다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const sideBar = await findByText('side bar');
+      const sideBarButton = await findByLabelText('button');
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).toBeVisible();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).not.toBeVisible();
+    });
+
+    it('side bar show 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙으로 이동된다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const contentArea = await findByText('content area');
+      const sideBarButton = await findByLabelText('button');
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(contentArea).toBeLeftOfScreenX();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(contentArea).toBeCenterOfScreenX();
+    });
+
+    it('side bar hide 상태에서 side bar 버튼이 비활성화되면 버튼을 클릭해도 작동하지 않는다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const sideBar = await findByText('side bar');
+      const contentArea = await findByText('content area');
+      const sideBarButton = await findByLabelText('button');
+
+      const { result } = renderHook(() => useSideBarStore());
+      result.current.deactiveSideBarButton();
+
+      expect(sideBar).not.toBeVisible();
+      expect(contentArea).toBeCenterOfScreenX();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).not.toBeVisible();
+      expect(contentArea).toBeCenterOfScreenX();
+    });
+
+    it('side bar show 상태에서 side bar 버튼이 비활성화되면 content area가 x축 기준 화면 중앙으로 이동하면서 side bar가 화면에서 사라지고, 버튼을 클릭해도 작동하지 않는다.', async () => {
+      const { findByText, findByLabelText } = render(<IntegrationComponent />);
+
+      const sideBar = await findByText('side bar');
+      const contentArea = await findByText('content area');
+      const sideBarButton = await findByLabelText('button');
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).toBeVisible();
+      expect(contentArea).toBeLeftOfScreenX();
+
+      const { result } = renderHook(() => useSideBarStore());
+      result.current.deactiveSideBarButton();
+
+      expect(sideBar).not.toBeVisible();
+      expect(contentArea).toBeCenterOfScreenX();
+
+      sideBarButton?.click();
+      await sleep(1000);
+
+      expect(sideBar).not.toBeVisible();
+      expect(contentArea).toBeCenterOfScreenX();
+    });
   });
 });
--- frontend/src/utils/matcher.ts
+++ frontend/src/utils/matcher.ts
@@ -5,17 +5,28 @@ type MatcherResult = {
 
 export interface CustomMatchers<R = unknown> {
   toBeCenterOfScreenX(): R;
+  toBeLeftOfScreenX(): R;
+}
+
+const ALLOWABLE_MARGIN = 100;
+
+function compare(a: number, b: number): number {
+  if (a < b - ALLOWABLE_MARGIN) {
+    return -1;
+  } else if (a > b + ALLOWABLE_MARGIN) {
+    return 1;
+  }
+  return 0;
 }
 
 export function toBeCenterOfScreenX(received: HTMLElement): MatcherResult {
-  const element = received as unknown as HTMLElement;
-  const rect = element.getBoundingClientRect();
+  const rect = received.getBoundingClientRect();
   const middleOfRectX = rect.left + rect.width / 2;
 
   const screenWidth = window.innerWidth || document.documentElement.clientWidth;
   const middleOfScreenX = screenWidth / 2;
 
-  const pass = middleOfScreenX - 100 < middleOfRectX && middleOfRectX < middleOfScreenX + 100;
+  const pass = compare(middleOfRectX, middleOfScreenX) === 0;
 
   if (pass) {
     return {
@@ -25,6 +36,28 @@ export function toBeCenterOfScreenX(received: HTMLElement): MatcherResult {
   } else {
     return {
       message: () => `expected to be centered horizontally`,
+      pass: false,
+    };
+  }
+}
+
+export function toBeLeftOfScreenX(received: HTMLElement): MatcherResult {
+  const rect = received.getBoundingClientRect();
+  const middleOfRectX = rect.left + rect.width / 2;
+
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+  const middleOfScreenX = screenWidth / 2;
+
+  const pass = compare(middleOfRectX, middleOfScreenX) === -1;
+
+  if (pass) {
+    return {
+      message: () => `expected not to be left of screen`,
+      pass: true,
+    };
+  } else {
+    return {
+      message: () => `expected to be left of screen`,
       pass: false,
     };
   }
