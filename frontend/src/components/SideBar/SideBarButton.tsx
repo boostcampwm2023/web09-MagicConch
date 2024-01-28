b19e1a7b2e021102f1402871e636cb18c08590f5
HeoJiye <heoze1031@gmail.com>
2024-01-28 14:07:00 +0900

    feat: side bar 버튼이 비활성화된 상태이고 sideBarState가 false인 상태에서 side bar 버튼을 클릭하면, sideBarState가  바뀌지 않는다.

--- frontend/src/components/SideBar/SideBarButton.tsx
+++ frontend/src/components/SideBar/SideBarButton.tsx
@@ -3,9 +3,10 @@ import { useEffect } from 'react';
 import { initSideBarStore, useSideBarStore } from '@stores/zustandStores/useSideBarStore';
 
 export default function SideBarButton() {
-  const { toggleSideBarState } = useSideBarStore();
+  const { sideBarButtonState, toggleSideBarState } = useSideBarStore();
 
   const handleButtonClick = () => {
+    if (!sideBarButtonState) return;
     toggleSideBarState();
   };
 
