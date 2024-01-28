63b74f6b33043645ba9a3cb8d6963a3674a62e13
kimyu0218 <serena35@ewhain.net>
2024-01-29 00:03:08 +0900

    refactor: sqlite 메모리 DB를 이용하여 e2e 테스트 진행
    - 기존에 작성한 TarotService mock 필요하지 않음
    - 테스트 모듈을 만들 때, sqlite를 사용하도록 설정함
    - 메모리 DB를 이용하기 때문에 EntityManager를 이용하여 데이터를
      조작하도록 구현

--- backend/was/test/tarot.e2e-spec.ts
+++ backend/was/test/tarot.e2e-spec.ts
@@ -1,26 +1,35 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { invalidResultId, resultId } from 'src/tarot/__mocks__/tarot-result';
-import { TarotService as MockedTarotService } from 'src/tarot/__mocks__/tarot.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BUCKET_URL } from 'src/common/constants/etc';
+import { TarotCard, TarotCardPack, TarotResult } from 'src/tarot/entities';
 import { TarotController } from 'src/tarot/tarot.controller';
 import { TarotService } from 'src/tarot/tarot.service';
 import * as request from 'supertest';
+import { EntityManager } from 'typeorm';
 
 describe('Tarot', () => {
   let app: INestApplication;
-  let tarotService: MockedTarotService;
+  let entityManager: EntityManager;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [__dirname + '/../src/**/entities/*.entity.{js,ts}'],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([TarotCard, TarotResult, TarotCardPack]),
+      ],
       controllers: [TarotController],
       providers: [TarotService],
-    })
-      .overrideProvider(TarotService)
-      .useClass(MockedTarotService)
-      .compile();
+    }).compile();
 
     app = moduleRef.createNestApplication();
-    tarotService = moduleRef.get<MockedTarotService>(TarotService);
+    entityManager = moduleRef.get(EntityManager);
+
     await app.init();
   });
 
@@ -29,42 +38,75 @@ describe('Tarot', () => {
   });
 
   describe('GET /tarot/card/:cardNo', () => {
-    it('[올바른 카드 번호] GET /tarot/card/0', () => {
-      return request(app.getHttpServer())
-        .get('/tarot/card/0')
-        .expect(200)
-        .expect(tarotService.findTarotCardByCardNo(0));
+    beforeAll(async () => {
+      const tarotCard: TarotCard = new TarotCard();
+      tarotCard.cardNo = 0;
+      tarotCard.ext = '.jpg';
+      await entityManager.save(tarotCard);
     });
 
-    it('[정수형이 아닌 카드 번호] GET /tarot/card/invalidCardNo', () => {
-      return request(app.getHttpServer())
-        .get('/tarot/card/invalidCardNo')
-        .expect(400);
+    describe('성공', () => {
+      it('[올바른 카드 번호] GET /tarot/card/0', () => {
+        return request(app.getHttpServer())
+          .get('/tarot/card/0')
+          .expect(200)
+          .expect((res) =>
+            expect(res.body).toEqual({ cardUrl: `${BUCKET_URL}/basic/0.jpg` }),
+          );
+      });
     });
 
-    it('[존재하지 않는 카드 번호] GET /tarot/card/-1', () => {
-      return request(app.getHttpServer()).get('/tarot/card/-1').expect(404);
+    describe('실패', () => {
+      it('[정수형이 아닌 카드 번호] GET /tarot/card/invalidCardNo', () => {
+        return request(app.getHttpServer())
+          .get('/tarot/card/invalidCardNo')
+          .expect(400);
+      });
+
+      it('[존재하지 않는 카드 번호] GET /tarot/card/-1', () => {
+        return request(app.getHttpServer()).get('/tarot/card/-1').expect(404);
+      });
     });
   });
 
   describe('GET /tarot/result/:id', () => {
-    it(`[올바른 아이디] GET /tarot/result/${resultId}`, () => {
-      return request(app.getHttpServer())
-        .get(`/tarot/result/${resultId}`)
-        .expect(200)
-        .expect(tarotService.findTarotResultById(resultId));
+    const id: string = '12345678-1234-5678-1234-567812345678';
+    const wrongId: string = '12345678-1234-5678-1234-567812345679';
+
+    beforeAll(async () => {
+      const tarotResult: TarotResult = new TarotResult();
+      tarotResult.id = id;
+      tarotResult.cardUrl = `${BUCKET_URL}/basic/0.jpg`;
+      tarotResult.message = '0번 타로 카드에 대한 해설입니다';
+      await entityManager.save(tarotResult);
     });
 
-    it('[UUID 형식이 아닌 아이디] GET /tarot/result/invalidUUID', () => {
-      return request(app.getHttpServer())
-        .get('/tarot/result/invalidUUID')
-        .expect(400);
+    describe('성공', () => {
+      it(`[올바른 아이디] GET /tarot/result/${id}`, () => {
+        return request(app.getHttpServer())
+          .get(`/tarot/result/${id}`)
+          .expect(200)
+          .expect((res) =>
+            expect(res.body).toEqual({
+              cardUrl: `${BUCKET_URL}/basic/0.jpg`,
+              message: '0번 타로 카드에 대한 해설입니다',
+            }),
+          );
+      });
     });
 
-    it(`[존재하지 않는 아이디] GET /tarot/result/${invalidResultId}`, () => {
-      return request(app.getHttpServer())
-        .get(`/tarot/result/${invalidResultId}`)
-        .expect(404);
+    describe('실패', () => {
+      it('[UUID 형식이 아닌 아이디] GET /tarot/result/invalidUUID', () => {
+        return request(app.getHttpServer())
+          .get('/tarot/result/invalidUUID')
+          .expect(400);
+      });
+
+      it(`[존재하지 않는 아이디] GET /tarot/result/${wrongId}`, () => {
+        return request(app.getHttpServer())
+          .get(`/tarot/result/${wrongId}`)
+          .expect(404);
+      });
     });
   });
 });
