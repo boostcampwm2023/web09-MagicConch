7016ac39785cd5bbb27ac7b93a4c5b6fad23c5e1
HeoJiye <heoze1031@gmail.com>
2024-01-28 12:07:08 +0900

    refactor: sidebar 통합 테스트 리팩토링
    senario별로 객체를 만들고, 테스트 코드를 재활용하는 식으로 리팩토링함.
    다음 페이지를 참고함: https://dev.to/manuartero/testing-a-react-context-provider-5cfg

--- frontend/src/components/SideBar/SideBar.integration.spec.tsx
+++ frontend/src/components/SideBar/SideBar.integration.spec.tsx
@@ -54,139 +54,140 @@ describe('SideBar 관련 컴포넌트 통합 테스트', () => {
       expect(contentArea).toBeCenterOfScreenX();
     });
 
-    it('애니메이션 효과는 발생하지 않는다.', async () => {
+    it('애니메이션 효과는 발생하지 않는다. (시간이 지나도 상태가 그대로이다)', async () => {
       const { findByText } = render(<IntegrationComponent />);
-
-      await sleep(1000);
 
       const sideBar = await findByText('side bar');
       const contentArea = await findByText('content area');
+
+      expect(sideBar).not.toBeVisible();
+      expect(contentArea).toBeCenterOfScreenX();
+
+      await sleep(1000);
 
       expect(sideBar).not.toBeVisible();
       expect(contentArea).toBeCenterOfScreenX();
     });
   });
 
-  describe('상태가 바뀌는 경우', () => {
-    it('side bar hide 상태에서 side bar 버튼을 클릭하면, side bar가 화면에 보여진다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
+  describe('side bar 버튼을 클릭해서 상태가 바뀌는 경우', () => {
+    [
+      {
+        scenario: 'side bar hide 상태에서 side bar 버튼을 클릭하면, side bar가 화면에 보여진다.',
+        initialState: {
+          sideBarState: false,
+          sideBarButtonState: true,
+        },
+        expected: {
+          sideBarVisible: true,
+        },
+      },
+      {
+        scenario:
+          'side bar hide 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙에서 왼쪽으로 이동된다.',
+        initialState: {
+          sideBarState: false,
+          sideBarButtonState: true,
+        },
+        expected: {
+          contentAreaPosX: 'left',
+        },
+      },
+      {
+        scenario: 'side bar show 상태에서 side bar 버튼을 클릭하면, side bar는 화면에서 사라진다.',
+        initialState: {
+          sideBarState: true,
+          sideBarButtonState: true,
+        },
+        expected: {
+          sideBarVisible: false,
+        },
+      },
+      {
+        scenario: 'side bar show 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙으로 이동된다.',
+        initialState: {
+          sideBarState: true,
+          sideBarButtonState: true,
+        },
+        expected: {
+          contentAreaPosX: 'center',
+        },
+      },
+      {
+        scenario: 'side bar hide 상태에서 side bar 버튼이 비활성화되면 버튼을 클릭해도 작동하지 않는다.',
+        initialState: {
+          sideBarState: false,
+          sideBarButtonState: false,
+        },
+        expected: {
+          sideBarVisible: false,
+          contentAreaPosX: 'center',
+        },
+        loopCount: 2,
+      },
+      {
+        scenario:
+          'side bar show 상태에서 side bar 버튼이 비활성화되면 content area가 x축 기준 화면 중앙으로 이동하면서 side bar가 화면에서 사라지고, 버튼을 클릭해도 작동하지 않는다.',
+        initialState: {
+          sideBarState: true,
+          sideBarButtonState: false,
+        },
+        expected: {
+          sideBarVisible: false,
+          contentAreaPosX: 'left',
+        },
+        loopCount: 2,
+      },
+    ].forEach(({ scenario, initialState, expected, loopCount = 1 }) => {
+      it(scenario, async () => {
+        const { findByText, findByLabelText } = render(<IntegrationComponent />);
+        const { result } = renderHook(() => useSideBarStore());
 
-      const sideBar = await findByText('side bar');
-      const sideBarButton = await findByLabelText('button');
+        const sideBar = await findByText('side bar');
+        const contentArea = await findByText('content area');
+        const sideBarButton = await findByLabelText('button');
 
-      result.current.hideSideBar();
-      await sleep(1000);
+        // 초기 store 설정
+        if (initialState.sideBarState) {
+          result.current.showSideBar();
+        } else {
+          result.current.hideSideBar();
+        }
+        expect(result.current.sideBarState).toBe(initialState.sideBarState);
 
-      expect(sideBar).not.toBeVisible();
+        if (initialState.sideBarButtonState) {
+          result.current.activeSideBarButton();
+        } else {
+          result.current.deactiveSideBarButton();
+        }
+        expect(result.current.sideBarButtonState).toBe(initialState.sideBarButtonState);
 
-      sideBarButton?.click();
-      await sleep(1000);
+        await sleep(1000);
 
-      expect(sideBar).toBeVisible();
-    });
+        // 테스트 시작 (loopCount 만큼 반복해서 테스트)
+        for (let i = 0; i < loopCount; i++) {
+          // 클릭 이벤트 발생
+          sideBarButton?.click();
+          await sleep(1000);
 
-    it('side bar hide 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙에서 왼쪽으로 이동된다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
+          // 예상한 상태가 맞는지 확인
+          if (expected.sideBarVisible != undefined) {
+            if (expected.sideBarVisible) {
+              expect(sideBar).toBeVisible();
+            } else {
+              expect(sideBar).not.toBeVisible();
+            }
+          }
 
-      const contentArea = await findByText('content area');
-      const sideBarButton = await findByLabelText('button');
-
-      result.current.hideSideBar();
-      await sleep(1000);
-
-      expect(contentArea).toBeCenterOfScreenX();
-
-      sideBarButton?.click();
-      await sleep(1000);
-
-      expect(contentArea).toBeLeftOfScreenX();
-    });
-
-    it('side bar show 상태에서 side bar 버튼을 클릭하면, side bar는 화면에서 사라진다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
-
-      const sideBar = await findByText('side bar');
-      const sideBarButton = await findByLabelText('button');
-
-      result.current.showSideBar();
-      await sleep(1000);
-
-      expect(sideBar).toBeVisible();
-
-      sideBarButton?.click();
-      await sleep(1000);
-
-      expect(sideBar).not.toBeVisible();
-    });
-
-    it('side bar show 상태에서 side bar 버튼을 클릭하면, content area는 x축 기준 화면 중앙으로 이동된다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
-
-      const contentArea = await findByText('content area');
-      const sideBarButton = await findByLabelText('button');
-
-      result.current.showSideBar();
-      await sleep(1000);
-
-      expect(contentArea).toBeLeftOfScreenX();
-
-      sideBarButton?.click();
-      await sleep(1000);
-
-      expect(contentArea).toBeCenterOfScreenX();
-    });
-
-    it('side bar hide 상태에서 side bar 버튼이 비활성화되면 버튼을 클릭해도 작동하지 않는다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
-
-      const sideBar = await findByText('side bar');
-      const contentArea = await findByText('content area');
-      const sideBarButton = await findByLabelText('button');
-
-      result.current.hideSideBar();
-      await sleep(1000);
-
-      expect(sideBar).not.toBeVisible();
-      expect(contentArea).toBeCenterOfScreenX();
-
-      result.current.deactiveSideBarButton();
-
-      sideBarButton?.click();
-      await sleep(1000);
-
-      expect(sideBar).not.toBeVisible();
-      expect(contentArea).toBeCenterOfScreenX();
-    });
-
-    it('side bar show 상태에서 side bar 버튼이 비활성화되면 content area가 x축 기준 화면 중앙으로 이동하면서 side bar가 화면에서 사라지고, 버튼을 클릭해도 작동하지 않는다.', async () => {
-      const { findByText, findByLabelText } = render(<IntegrationComponent />);
-      const { result } = renderHook(() => useSideBarStore());
-
-      const sideBar = await findByText('side bar');
-      const contentArea = await findByText('content area');
-      const sideBarButton = await findByLabelText('button');
-
-      result.current.showSideBar();
-      await sleep(1000);
-
-      expect(sideBar).toBeVisible();
-      expect(contentArea).toBeLeftOfScreenX();
-
-      result.current.deactiveSideBarButton();
-
-      expect(sideBar).not.toBeVisible();
-      expect(contentArea).toBeCenterOfScreenX();
-
-      sideBarButton?.click();
-      await sleep(1000);
-
-      expect(sideBar).not.toBeVisible();
-      expect(contentArea).toBeCenterOfScreenX();
+          if (expected.contentAreaPosX != undefined) {
+            if (expected.contentAreaPosX === 'center') {
+              expect(contentArea).toBeCenterOfScreenX();
+            } else {
+              expect(contentArea).toBeLeftOfScreenX();
+            }
+          }
+        }
+      });
     });
   });
 });
