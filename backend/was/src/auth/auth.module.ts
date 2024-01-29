fc9cba22a83ee3d6816c37ed6f9dc9373cfee3bc
kimyu0218 <serena35@ewhain.net>
2024-01-30 03:09:39 +0900

    refactor: MembersService를 대신 MembersRepository 주입 받기
    - Member 객체는 Member 모듈에서 관리하는 게 맞다고 판단하여
      MembersService에서 조작하도록 구현했었다
    - 하지만 사용자 관련 로직이 늘어나면 Auth 모듈에서 방대한
      MembersService를 주입 받게 된다
    - 이는 Auth 모듈에서 사용하지 않는 기능들까지 공개되는 것이기 때문에
      오용의 가능성이 있다고 판단했다
    - MembersService 대신 MembersRepository를 주입 받아 Auth 모듈에서 직접
      Member 객체를 조작하도록 수정했다

--- backend/was/src/auth/auth.module.ts
+++ backend/was/src/auth/auth.module.ts
@@ -2,20 +2,17 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/members/entities';
-import { MembersModule } from 'src/members/members.module';
-import { MembersService } from 'src/members/members.service';
 import { AuthController } from './auth.controller';
 import { JwtAuthGuard, SocketJwtAuthGuard } from './guard';
 import { AuthService, KakaoAuthService } from './service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [PassportModule, TypeOrmModule.forFeature([Member]), MembersModule],
+  imports: [PassportModule, TypeOrmModule.forFeature([Member])],
   controllers: [AuthController],
   providers: [
     AuthService,
     KakaoAuthService,
-    MembersService,
     JwtStrategy,
     JwtAuthGuard,
     SocketJwtAuthGuard,
