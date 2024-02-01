af153e0d8bd7731723055e5e35a4f17e2aa3f08d
kimyu0218 <serena35@ewhain.net>
2024-02-01 16:24:33 +0900

    docs: Auth API swagger 문서 수정
    새롭게 추가된 API의 문서 작성

--- backend/was/src/auth/auth.decorators.ts
+++ backend/was/src/auth/auth.decorators.ts
@@ -1,5 +1,13 @@
 import { SwaggerDecoratorBuilder } from '@kimyu0218/swagger-decorator-builder';
 
+export const AuthenticateDecorator = () =>
+  new SwaggerDecoratorBuilder()
+    .setOperation({ summary: '로그인 여부 확인' })
+    .removeResponse(401)
+    .removeResponse(403)
+    .removeResponse(404)
+    .build();
+
 export const KakaoLoginDecorator = () =>
   new SwaggerDecoratorBuilder()
     .setOperation({ summary: '카카오 로그인' })
@@ -8,15 +16,15 @@ export const KakaoLoginDecorator = () =>
       type: 'string',
       description: '카카오 로그인의 인가 코드',
     })
-    .addResponse(400)
     .removeResponse(403)
     .removeResponse(404)
+    .addResponse(400)
     .build();
 
 export const LogoutDecorator = () =>
   new SwaggerDecoratorBuilder()
     .setOperation({ summary: '로그아웃' })
-    .addResponse(400)
     .removeResponse(403)
     .removeResponse(404)
+    .addResponse(400)
     .build();
