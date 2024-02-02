a91943ca2d870ef61c2a749bf8cf50459accb6c2
Song_Minhyung <song961003@gmail.com>
2024-02-02 13:16:25 +0900

    test: useSignalingSocket 테스트 유저행동 기반으로 수정
    - 기존에는 함수의 입출력 결과만 확인했는데 이러면 테스트의 수정이 빈번할것같음
    - 그래서 이번에는 유저행동 기반으로 테스트를 작성함

---
+++ frontend/src/utils/__mocks__/random.ts
@@ -0,0 +1,9 @@
+// export function randomString(length: number = 8) {
+//   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   return chars.slice(0, length);
+// }
+
+export const randomString = vi.fn().mockImplementation((length: number = 8) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return chars.slice(0, length);
+});
