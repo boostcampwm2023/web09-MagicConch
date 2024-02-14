7621fc6a6d7a00b28899d6261afda733e4746a40
HeoJiye <heoze1031@gmail.com>
2024-02-14 14:20:21 +0900

    feat: 로그인 여부, 챗 로그 리스트 react-query 작성

--- frontend/src/stores/queries/getAuthorizedQuery.ts
+++ frontend/src/stores/queries/getAuthorizedQuery.ts
@@ -1,16 +1,13 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
+export interface AuthorizedResponse {
+  isAuthenticated: boolean;
+}
+
 export function getAuthorizedQuery() {
   return useSuspenseQuery({
     queryKey: [`authorizedQueryKey`],
-    queryFn: async () => {
-      try {
-        const response = await axios.get(`${import.meta.env.VITE_WAS_URL}/oauth/authenticated`);
-        return response.status === 200;
-      } catch {
-        return false;
-      }
-    },
+    queryFn: async () => await axios.get<AuthorizedResponse>(`${import.meta.env.VITE_WAS_URL}/oauth/authenticated`),
   });
 }
--- frontend/src/stores/queries/getChatLogListQuery.ts
+++ frontend/src/stores/queries/getChatLogListQuery.ts
@@ -1,12 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export type ChatLogListResponse = ChatLog[];
-
-interface ChatLog {
+export type ChatLogListResponse = {
   id: string;
   title: string;
-}
+}[];
 
 export function getChatLogListQuery() {
   return useSuspenseQuery({
