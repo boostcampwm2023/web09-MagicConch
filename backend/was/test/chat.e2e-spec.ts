7f56070901287c84e1db48c045dda04dc1b26de2
kimyu0218 <serena35@ewhain.net>
2024-02-16 21:41:56 +0900

    refactor: 엔티티 수정에 따른 테스트 데이터 수정

--- backend/was/test/chat.e2e-spec.ts
+++ backend/was/test/chat.e2e-spec.ts
@@ -138,16 +138,19 @@ describe('Chat', () => {
         {
           isHost: true,
           message: '어떤 고민이 있어?',
+          order: 1,
         },
         {
           isHost: false,
           message: '오늘 운세를 알고 싶어',
+          order: 2,
         },
       ];
       for (const chatLog of chatLogs) {
         const message: ChattingMessage = new ChattingMessage();
         message.isHost = chatLog.isHost;
         message.message = chatLog.message;
+        message.order = chatLog.order;
         message.room = savedRoom;
         await entityManager.save(message);
         messages.push(ChattingMessageDto.fromEntity(message));
