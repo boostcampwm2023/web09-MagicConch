6c9f14b5c685832942265bdb56be0dabbb676b95
HeoJiye <heoze1031@gmail.com>
2024-02-21 16:15:56 +0900

    feat: gtm 모듈 대신에 index.html에 세팅

--- frontend/src/business/services/TOLD.ts
+++ frontend/src/business/services/TOLD.ts
@@ -1,11 +1,3 @@
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-  gtmId: 'GTM-55JJ73F3',
-};
-
-TagManager.initialize(tagManagerArgs);
-
 const TOLD = {
   displayForm: (type: string) => (window as any).dataLayer.push({ event: type }),
   isFormVisible: () => {
