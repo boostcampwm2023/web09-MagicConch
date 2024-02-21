e8654913420e4ad330128986ddad4428526f8f7e
HeoJiye <heoze1031@gmail.com>
2024-02-21 17:13:46 +0900

    remove: displayForm 로그 삭제

--- frontend/src/business/services/TOLD.ts
+++ frontend/src/business/services/TOLD.ts
@@ -1,12 +1,15 @@
 const TOLD = {
   displayForm: (type: string) => {
     const button = document.createElement('button');
+
     button.innerText = type;
     button.style.display = 'none';
+
     document.body.appendChild(button);
+
     button.click();
+
     document.body.removeChild(button);
-    console.log('displayForm', type);
   },
   isFormVisible: () => {
     const container = document.querySelector('#told-container');
