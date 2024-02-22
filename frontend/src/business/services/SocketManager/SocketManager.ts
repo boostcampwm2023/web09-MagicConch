db88cb29e0d99c38de95f7164a04c71f79db4e59
HeoJiye <heoze1031@gmail.com>
2024-02-22 17:58:32 +0900

    feat: frontend 쪽 socket 이벤트 타입 적용

--- frontend/src/business/services/SocketManager/SocketManager.ts
+++ frontend/src/business/services/SocketManager/SocketManager.ts
@@ -1,7 +1,11 @@
 import { Socket, io } from 'socket.io-client';
 
-export class SocketManager {
-  #socket: Socket | undefined;
+interface EventsMap {
+  [event: string]: any;
+}
+
+export class SocketManager<T1 extends EventsMap, T2 extends EventsMap> {
+  #socket: Socket<T1, T2> | undefined;
 
   #url: string;
   #path?: string;
@@ -35,17 +39,17 @@ export class SocketManager {
     this.#socket = undefined;
   }
 
-  on<U>(eventName: string, eventListener: (args: U) => void) {
+  on(...params: Parameters<Socket['on']>) {
     if (!this.socket) {
       throw new Error('소켓이 존재하지 않습니다.');
     }
-    this.socket.on(eventName, eventListener);
+    this.socket.on(...params);
   }
 
-  emit(eventName: string, ...eventArgs: unknown[]) {
+  emit(...params: Parameters<Socket['emit']>) {
     if (!this.socket) {
       throw new Error('소켓이 존재하지 않습니다.');
     }
-    this.socket.emit(eventName, ...eventArgs);
+    this.socket.emit(...params);
   }
 }
