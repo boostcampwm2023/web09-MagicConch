96e29c42370174cd566c63e8e08fa932b7ee6f53
kimyu0218 <serena35@ewhain.net>
2024-02-22 20:48:48 +0900

    refactor: FORBIDDEN 하나로 합치기

--- backend/was/src/exceptions/codemap/chat-codemap.ts
+++ backend/was/src/exceptions/codemap/chat-codemap.ts
@@ -7,18 +7,11 @@ export const CHAT_CODEMAP: ExceptionCodemap = {
     code: 'MCE001',
     description: '요청에 포함된 채팅방 아이디가 존재하지 않습니다.',
   },
-  ROOM_UPDATE_FORBIDDEN: {
+  ROOM_FORBIDDEN: {
     status: 403,
-    message: '채팅방을 수정할 수 없습니다.',
+    message: '채팅방을 수정/삭제할 수 없습니다.',
     code: 'MCE002',
     description:
-      '요청한 사용자가 해당 채팅방의 수정 권한을 가지고 있지 않습니다.',
-  },
-  ROOM_DELETE_FORBIDDEN: {
-    status: 403,
-    message: '채팅방을 삭제할 수 없습니다.',
-    code: 'MCE003',
-    description:
-      '요청한 사용자가 해당 채팅방의 삭제 권한을 가지고 있지 않습니다.',
+      '요청한 사용자가 해당 채팅방의 수정/삭제 권한을 가지고 있지 않습니다.',
   },
 };
