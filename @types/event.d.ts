64169261ed3d29d680c55f21733e2dc571e048a8
HeoJiye <heoze1031@gmail.com>
2024-02-23 09:30:43 +0900

    feat: human socket 이벤트 타입 선언 (임시로 주석 처리)

--- @types/event.d.ts
+++ @types/event.d.ts
@@ -16,8 +16,28 @@ interface AiSocketEvent {
 }
 
 interface HumanSocketEvent {
-  ServerToClientEvent: {};
-  ClientToServerEvent: {};
+  ServerToClientEvent: DefaultEventsMap;
+  // {
+  //   connection: (data: { description?: RTCSessionDescription; candidate?: RTCIceCandidate }) => void;
+  //   welcome: () => void;
+  //   userExit: () => void;
+  //   hostExit: () => void;
+  //   roomCreated: () => void;
+  //   roomNameGenerated: (roomId: string) => void;
+  //   joinRoomFailed: () => void;
+  //   joinRoomSuccess: () => void;
+  //   roomExist: () => void;
+  //   roomNotExist: () => void;
+  //   roomFull: () => void;
+  // };
+  ClientToServerEvent: DefaultEventsMap;
+  // {
+  //   connection: (data: { description?: RTCSessionDescription; candidate?: RTCIceCandidate; roomName: string }) => void;
+  //   generateRoomName: () => void;
+  //   createRoom: (roomId: string, password: string) => void;
+  //   joinRoom: (roomId: string, password: string) => void;
+  //   checkRoomExist: (roomName: string) => void;
+  // };
   InterServerEvents: {};
   SocketData: {};
 }
