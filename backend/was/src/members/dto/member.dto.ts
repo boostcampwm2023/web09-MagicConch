00603700728e4b6b20ffe50a5a2b7dbc6d1a1a08
kimyu0218 <serena35@ewhain.net>
2024-03-06 19:12:56 +0900

    feat: swagger 문서를 위한 정보 작성

--- backend/was/src/members/dto/member.dto.ts
+++ backend/was/src/members/dto/member.dto.ts
@@ -1,9 +1,11 @@
+import { PartialType } from '@nestjs/swagger';
 import { Member } from '../entities';
+import { CreateMemberDto } from './create-member.dto';
 
-export class MemberDto {
+export class MemberDto extends PartialType(CreateMemberDto) {
   static fromEntity(entity: Member): MemberDto {
     return {
-      nickname: entity.nickname,
+      nickname: entity.nickname ?? '',
       profileUrl: entity.profileUrl,
     };
   }
