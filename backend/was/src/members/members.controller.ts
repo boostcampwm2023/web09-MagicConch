60ae51def3ccb7750bacb041433da1819d1874cd
kimyu0218 <serena35@ewhain.net>
2024-03-06 19:18:33 +0900

    feat: swagger 문서를 위한 데코레이터 추가

--- backend/was/src/members/members.controller.ts
+++ backend/was/src/members/members.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '@auth/guard';
 import { MemberDto } from './dto/member.dto';
+import { FindMemberByEmailDecorator } from './members.decorators';
 import { MembersService } from './members.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('/members')
+@ApiTags('✅ Members API')
 export class MembersController {
   constructor(private readonly membersService: MembersService) {}
 
   @Get()
+  @FindMemberByEmailDecorator('사용자 정보', MemberDto)
   async findMemberByEmail(@Req() req: any): Promise<MemberDto> {
     return await this.membersService.findMemberByEmail(
       req.user.email,
