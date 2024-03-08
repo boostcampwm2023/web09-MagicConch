fcd380d1eb02a8817a5aa94ce369005e33dd995d
HeoJiye <heoze1031@gmail.com>
2024-03-08 16:55:11 +0900

    design: rightItem 여러개일 때 고려해서 스타일 수정

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -8,14 +8,11 @@ export function Header({ rightItems }: HeaderProps) {
   return (
     <header className="sticky w-full h-48 flex justify-between items-center surface-content text-default px-8 py-5 z-50">
       <LogoButton />
-      {rightItems?.map((item, index) => (
-        <div
-          className="flex gap-16"
-          key={index}
-        >
-          {item}
-        </div>
-      ))}
+      <div className="flex-with-center gap-16">
+        {rightItems?.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
     </header>
   );
 }
