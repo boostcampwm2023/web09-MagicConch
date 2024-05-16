0f97351b56ee1ba47264663f86f973798b9d08c4
kimyu0218 <serena35@ewhain.net>
2024-05-16 23:02:13 +0900

    refactor: sentry 버전 변경 사항 반영

--- backend/was/src/common/config/sentry.setting.ts
+++ backend/was/src/common/config/sentry.setting.ts
@@ -1,16 +1,14 @@
 import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
 import * as Sentry from '@sentry/node';
-import { ProfilingIntegration } from '@sentry/profiling-node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 export function setupSentry(app: INestApplication, dsn: string): void {
   Sentry.init({
     dsn: dsn,
-    integrations: [new ProfilingIntegration()],
+    integrations: [nodeProfilingIntegration()],
     tracesSampleRate: 0.3,
     profilesSampleRate: 0.3,
   });
 
-  app.use(Sentry.Handlers.errorHandler());
-  app.use(Sentry.Handlers.tracingHandler());
-  app.use(Sentry.Handlers.requestHandler());
+  Sentry.setupExpressErrorHandler(app);
 }
